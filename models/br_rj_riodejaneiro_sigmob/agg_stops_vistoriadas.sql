132a115b0b5badbb28765ea6317938351844372f
hellcassius <caiorogerio.santos@gmail.com>
2022-05-10 14:37:12 -0300

    fix formatting

--- models/br_rj_riodejaneiro_sigmob/agg_stops_vistoriadas.sql
+++ models/br_rj_riodejaneiro_sigmob/agg_stops_vistoriadas.sql
@@ -1,7 +1,12 @@
 WITH t AS (
 SELECT 
     data_versao,
-    case when json_value(content, '$.IDPropriedadeParada') is null then false else true end flag_vistoriada,
+    case 
+        when 
+        json_value(content, '$.IDPropriedadeParada') is null 
+        then false 
+    else true 
+    end flag_vistoriada,
     json_value(content, '$.AP') as AP,
     json_value(content, '$.RA') as RA,
     json_value(content, '$.Bairro') as Bairro
