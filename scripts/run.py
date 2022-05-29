d34768a8730ed4a99eac3ca790332239a9f0a8cf
Fernanda Scovino <fscovinom@gmail.com>
2022-05-29 14:13:54 -0300

    cchaneg run to test

--- scripts/run.py
+++ scripts/run.py
@@ -2,9 +2,10 @@ import os
 from datetime import datetime as dt
 from datetime import timedelta
 
-run_date = dt.strptime("2022-03-28", "%Y-%m-%d")
-for i in range(1, 8):
-    command = f"dbt run --profiles-dir . --vars \"{{'run_date':{run_date}}}\" --select " # --exclude sumario_viagem_completa
-    print("Running: ", command)
+run_date = dt.strptime("2022-05-16", "%Y-%m-%d")
+for i in range(0, 6):
+    date = run_date + timedelta(days=i)
+    print(f"\n=========> DATA: {date}")
+    command = f"dbt run --profiles-dir . --vars \"{{'run_date':{date}}}\" --select +viagem_completa" # --exclude sumario_viagem_completa
+    print("=========> Running: ", command)
     os.system(command)
-run_date += timedelta(days=7)
