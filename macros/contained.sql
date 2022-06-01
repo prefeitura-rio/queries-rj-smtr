9bbda560904e9fe7029a5bf0948d7dfc02abec78
hellcassius <caiorogerio.santos@gmail.com>
2022-06-01 17:37:38 -0300

    add whitespace control for readability of the compiled SQL

--- macros/contained.sql
+++ macros/contained.sql
@@ -8,12 +8,12 @@ from (
         SELECT DISTINCT
             {{column_name}}
         FROM {{model}}
-        {% if execute %}
-        {% set query ="SELECT MAX(" ~ partition_column ~ ") FROM " ~ model %}
-        {% set max_partition_date = run_query(query).columns[0].values()[0] %}
-        {{log(query, info=True)}}
-        {{log(max_partition_date, info=True)}}
-        {% endif %}
+        {% if execute -%}
+        {%- set query ="SELECT MAX(" ~ partition_column ~ ") FROM " ~ model -%}
+        {%- set max_partition_date = run_query(query).columns[0].values()[0] -%}
+        {{- log(query, info=True) -}}
+        {{- log(max_partition_date, info=True) -}}
+        {%- endif -%}
         WHERE {{partition_column}} = "{{max_partition_date}}"
     )
     LEFT JOIN (
