67acd93e38848e12291aeb1afa27f4f3ced5ba01
Caio R S dos Santos <caiorogerio.santos@gmail.com>
2022-06-07 20:29:36 -0300

    [sppo][brt] feat: add gps queries (#13)
    * first commit
    
    * rename to prevent naming conflict
    
    * add br_rj_riodejaneiro_veiculos
    
    * set vars inside queries, add config blocks
    
    * add filters with vars
    
    * standby versions until macro
    
    * adjust filters
    
    * add add filters to brt
    
    * trim whitespace from servico
    
    * organize vas
    
    * add version & adjust spaces
    
    Co-authored-by: Fernanda Scovino <fscovinom@gmail.com>

---
+++ models/br_rj_riodejaneiro_brt_gps/brt_aux_registros_filtrada.sql
@@ -0,0 +1,61 @@
+/*
+- Descrição:
+Filtragem e tratamento básico de registros de gps.
+1. Filtra registros antigos. Remove registros que tem diferença maior
+   que 1 minuto entre o timestamp_captura e timestamp_gps.
+2. Filtra registros que estão fora de uma caixa que contém a área do
+   município de Rio de Janeiro.
+*/
+WITH
+box AS (
+  /*1. Geometria de caixa que contém a área do município de Rio de Janeiro.*/ 
+	SELECT
+	*
+	FROM
+	{{ var('limites_caixa') }}),
+gps AS (
+  /* 1. Filtra registros antigos. Remove registros que tem diferença maior
+   que 1 minuto entre o timestamp_captura e timestamp_gps.*/
+  SELECT
+    *,
+    ST_GEOGPOINT(longitude, latitude) posicao_veiculo_geo
+  FROM
+    {{ ref('brt_registros_desaninhada') }}
+  {% if is_incremental() -%}
+  WHERE
+    data between DATE("{{var('date_range_start')}}") and DATE("{{var('date_range_end')}}")
+    AND timestamp_gps > "{{var('date_range_start')}}" and "{{var('date_range_end')}}"
+    AND DATETIME_DIFF(timestamp_captura, timestamp_gps, MINUTE) BETWEEN 0 AND 1
+  {%- endif %}
+    ),
+filtrada AS (
+  /* 2. Filtra registros que estão fora de uma caixa que contém a área do
+   município de Rio de Janeiro.*/
+  SELECT
+    id_veiculo,
+    latitude,
+    longitude,
+    posicao_veiculo_geo,
+    velocidade,
+    servico,
+    timestamp_gps,
+    timestamp_captura,
+    data,
+    hora,
+    row_number() over (partition by id_veiculo, timestamp_gps, servico) rn
+  FROM
+    gps
+  WHERE
+    ST_INTERSECTSBOX(posicao_veiculo_geo,
+      ( SELECT min_longitude FROM box),
+      ( SELECT min_latitude FROM box),
+      ( SELECT max_longitude FROM box),
+      ( SELECT max_latitude FROM box)) 
+  )
+SELECT
+  * except(rn),
+  {{ var("version") }} as versao
+FROM
+  filtrada
+WHERE
+  rn = 1
---
+++ models/br_rj_riodejaneiro_onibus_gps/sppo_aux_registros_flag_trajeto_correto.sql
@@ -0,0 +1,145 @@
+{{
+  config(
+    materialized='ephemeral'
+  )
+}}
+
+/*
+Descrição:
+Calcula se o veículo está dentro do trajeto correto dado o traçado (shape) cadastrado no SIGMOB em relação à linha que está sendo
+transmitida.
+1. Calcula as intersecções definindo um 'buffer', utilizado por st_dwithin para identificar se o ponto está à uma
+distância menor ou igual ao tamanho do buffer em relação ao traçado definido no SIGMOB.
+2. Calcula um histórico de intersecções nos ultimos 10 minutos de registros de cada carro. Definimos que o carro é
+considerado fora do trajeto definido se a cada 10 minutos, ele não esteve dentro do traçado planejado pelo menos uma
+vez.
+3. Identifica se a linha informada no registro capturado existe nas definições presentes no SIGMOB.
+4. Definimos em outra tabela uma 'data_versao_efetiva', esse passo serve tanto para definir qual versão do SIGMOB utilizaremos em 
+caso de falha na captura, quanto para definir qual versão será utilizada para o cálculo retroativo do histórico de registros que temos.
+5. Como não conseguimos identificar o itinerário que o carro está realizando, no passo counts, os resultados de
+intersecções são dobrados, devido ao fato de cada linha apresentar dois itinerários possíveis (ida/volta). Portanto,
+ao final, realizamos uma agregação LOGICAL_OR que é true caso o carro esteja dentro do traçado de algum dos itinerários
+possíveis para a linha informada.
+*/
+WITH
+  registros AS (
+    SELECT id_veiculo, linha, latitude, longitude, data, posicao_veiculo_geo, timestamp_gps
+    FROM
+      {{ ref('sppo_aux_registros_filtrada') }} r 
+    {% if not flags.FULL_REFRESH -%}
+    WHERE
+      data between DATE("{{var('date_range_start')}}") and DATE("{{var('date_range_end')}}")
+    AND timestamp_gps > "{{var('date_range_start')}}" and timestamp_gps <="{{var('date_range_end')}}"
+    AND DATETIME_DIFF(timestamp_captura, timestamp_gps, MINUTE) BETWEEN 0 AND 1
+    {%- endif -%}
+  ),
+  intersec AS (
+    SELECT
+      r.*,
+      s.data_versao,
+      s.linha_gtfs,
+      s.route_id,
+      -- 1. Buffer e intersecções
+      CASE
+        WHEN st_dwithin(shape, posicao_veiculo_geo, {{ var('tamanho_buffer_metros') }}) THEN TRUE
+        ELSE FALSE
+      END AS flag_trajeto_correto,
+      -- 2. Histórico de intersecções nos últimos 10 minutos a partir da timestamp_gps atual
+      CASE
+        WHEN 
+          COUNT(CASE WHEN st_dwithin(shape, posicao_veiculo_geo, {{ var('tamanho_buffer_metros') }}) THEN 1 END) 
+          OVER (PARTITION BY id_veiculo 
+                ORDER BY UNIX_SECONDS(TIMESTAMP(timestamp_gps)) 
+                RANGE BETWEEN {{ var('intervalo_max_desvio_segundos') }} PRECEDING AND CURRENT ROW) >= 1 
+          THEN True
+        ELSE False
+      END AS flag_trajeto_correto_hist,
+      -- 3. Identificação de cadastro da linha no SIGMOB
+      CASE WHEN s.linha_gtfs IS NULL THEN False ELSE True END AS flag_linha_existe_sigmob,
+    -- 4. Join com data_versao_efetiva para definição de quais shapes serão considerados no cálculo das flags
+    FROM (
+      SELECT 
+        t1.*,
+        t2.data_versao_efetiva_shapes as data_versao_efetiva
+      FROM registros t1
+      JOIN  {{ ref('data_versao_efetiva') }} t2
+      ON t1.data = t2.data
+    ) r
+    LEFT JOIN (
+      SELECT * 
+      FROM {{ ref('shapes_geom') }} 
+      WHERE id_modal_smtr in ({{ var('sppo_id_modal_smtr')|join(', ') }})
+      {% if not flags.FULL_REFRESH  %}
+      AND data_versao between DATE("{{var('date_range_start')}}") and DATE("{{var('date_range_end')}}")
+      {% endif %}
+    ) s
+    ON
+      r.linha = s.linha_gtfs
+    AND
+      r.data_versao_efetiva = s.data_versao
+  ),
+  flags as  (
+    -- 5. Agregação com LOGICAL_OR para evitar duplicação de registros
+    SELECT
+      id_veiculo,
+      linha,
+      linha_gtfs,
+      route_id,
+      data,
+      timestamp_gps,
+      LOGICAL_OR(flag_trajeto_correto) AS flag_trajeto_correto,
+      LOGICAL_OR(flag_trajeto_correto_hist) AS flag_trajeto_correto_hist,
+      LOGICAL_OR(flag_linha_existe_sigmob) AS flag_linha_existe_sigmob,
+    FROM intersec i
+    GROUP BY
+      id_veiculo,
+      linha,
+      linha_gtfs,
+      route_id,
+      data,
+      data_versao,
+      timestamp_gps
+  ),
+  counts as (
+  select 
+      id_veiculo,
+      timestamp_gps,
+      linha,
+      (case when flag_trajeto_correto is true then 3 else 0 end + 
+      case when flag_trajeto_correto_hist is true then 2 else 0 end + 
+      case when flag_linha_existe_sigmob is true then 1 else 0 end) most_true, 
+      count(linha) over (partition by id_veiculo, timestamp_gps) ct,
+      row_number() over (partition by id_veiculo, timestamp_gps) rn
+  from flags
+  ),
+  provavel as (
+    select 
+        id_veiculo,
+        timestamp_gps,
+        linha,
+        CASE
+          WHEN ct>1
+          THEN    
+              CASE 
+              WHEN most_true = max(most_true) over(partition by id_veiculo, timestamp_gps order by rn) 
+              AND lead(most_true) over(partition by id_veiculo, timestamp_gps order by rn) < max(most_true) over(
+                    partition by id_veiculo, timestamp_gps order by rn)
+              THEN linha 
+              WHEN most_true = lead(most_true) over(partition by id_veiculo, timestamp_gps order by rn)
+              THEN linha
+              ELSE lead(linha) over(partition by id_veiculo, timestamp_gps order by rn) end
+          WHEN ct = 1
+          THEN linha
+        END AS linha_provavel
+    FROM counts
+  )
+SELECT
+  f.*
+FROM
+  flags f
+JOIN
+  provavel P
+ON
+  f.id_veiculo = p.id_veiculo
+  AND f.timestamp_gps = p.timestamp_gps
+  AND f.linha = p.linha_provavel
