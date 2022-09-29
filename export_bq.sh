d815e61bc2b174feb1a4bac120345d5ce35c278d
Fernanda Scovino <fscovinom@gmail.com>
2022-09-29 13:27:07 -0300

    Revert "Merge branch 'master' into dev"
    This reverts commit e75dd981caaac8f6f3ce02baeaf9062c2c480aad, reversing
    changes made to 812fe3cf330cb484a9c1e279c803c9cb7a969f69.

--- export_bq.sh
+++
@@ -1,7 +0,0 @@
-bq extract 
---destination_format CSV
---compression GZIP
---field_delimiter ","
---print_header [true, false] 
-rj-smtr-dev:projeto_subsidio_sppo.registros_status_viagem$
-gs://[BUCKET]/part_col=[DATE]/[FILENAME]-*.[csv, json, avro]
