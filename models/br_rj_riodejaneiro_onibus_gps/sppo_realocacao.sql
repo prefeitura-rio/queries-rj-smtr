4f05c76f1b10f9ffc35ce3b4c42fb1197c888f7a
Fernanda Scovino <fscovinom@gmail.com>
2022-11-24 09:53:50 -0300

    fix timezone

--- models/br_rj_riodejaneiro_onibus_gps/sppo_realocacao.sql
+++ models/br_rj_riodejaneiro_onibus_gps/sppo_realocacao.sql
@@ -1,12 +1,12 @@
 SELECT 
   SAFE_CAST(id_veiculo AS STRING) id_veiculo,
-  SAFE_CAST(datetime_operacao AS DATETIME) datetime_operacao,
+  SAFE_CAST(DATETIME(TIMESTAMP(datetime_operacao), "America/Sao_Paulo") AS DATETIME) datetime_operacao,
   concat(
     ifnull(REGEXP_EXTRACT(servico, r'[A-Z]+'), ""), 
     ifnull(REGEXP_EXTRACT(servico, r'[0-9]+'), "") 
   ) as servico,
-  SAFE_CAST(datetime_entrada AS DATETIME) as datetime_entrada,
-  SAFE_CAST(datetime_saida AS DATETIME) as datetime_saida,
+  SAFE_CAST(DATETIME(TIMESTAMP(datetime_entrada), "America/Sao_Paulo") AS DATETIME) as datetime_entrada,
+  SAFE_CAST(DATETIME(TIMESTAMP(datetime_saida), "America/Sao_Paulo") AS DATETIME) as datetime_saida,
   SAFE_CAST(DATETIME(TIMESTAMP(timestamp_processamento), "America/Sao_Paulo") AS DATETIME) as timestamp_processamento,
   SAFE_CAST(DATETIME(TIMESTAMP(timestamp_captura), "America/Sao_Paulo") AS DATETIME) as timestamp_captura,
   data,
