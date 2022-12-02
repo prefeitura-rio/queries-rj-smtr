7a0b47cc0b0993a2658c3709081382e33248172c
Fernanda Scovino <fscovinom@gmail.com>
2022-12-01 22:53:12 -0300

    fix profiles folder

--- dev/utils.py
+++ dev/utils.py
@@ -13,7 +13,7 @@ def run_dbt_model(
     upstream: bool = None,
     downstream: bool = None,
     exclude: str = None,
-    flags: str = "-x --profiles-dir ./profiles-dev",
+    flags: str = "-x --profiles-dir ./dev",
     _vars: Union[dict, List[Dict]] = None,
 ):
     """
