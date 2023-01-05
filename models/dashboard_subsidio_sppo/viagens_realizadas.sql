29b775503dbb6bfbacd03e18e0e3d888b41dfe13
Fernanda Scovino <fscovinom@gmail.com>
2023-01-05 18:28:14 -0300

    change dash var to end_date

--- models/dashboard_subsidio_sppo/viagens_realizadas.sql
+++ models/dashboard_subsidio_sppo/viagens_realizadas.sql
@@ -3,4 +3,4 @@ SELECT
 FROM
   {{ ref("viagem_completa") }}
 WHERE
-  data BETWEEN "2022-06-01" AND DATE_SUB(DATE("{{ var("run_date") }}"), INTERVAL 1 DAY)
+  data BETWEEN "2022-06-01" AND DATE("{{ var("end_date") }}")
