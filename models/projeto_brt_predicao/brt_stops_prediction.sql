4fbaf44ecd9db531be31266a23fc30ca36d48937
Fernanda Scovino <fscovinom@gmail.com>
2023-01-11 19:04:23 -0300

    add median model

--- models/projeto_brt_predicao/brt_stops_prediction.sql
+++ models/projeto_brt_predicao/brt_stops_prediction.sql
@@ -1,1 +1,10 @@
 {# Calcula a predição de chegada no próximo ponto para todas as trips #}
+
+SELECT
+  DISTINCT stop_id_origin,
+  stop_id_destiny,
+  tipo_dia,
+  hora,
+  PERCENTILE_CONT(stop_interval, 0.5) OVER(PARTITION BY stop_id_origin, stop_id_destiny, tipo_dia, hora) AS delta_secs
+FROM
+ {{ ref("brt_stops_intervals") }}
