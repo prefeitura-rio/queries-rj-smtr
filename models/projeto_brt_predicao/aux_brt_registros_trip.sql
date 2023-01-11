0daeddcd11d971c2c3f072523043bbbf548c984d
Fernanda Scovino <fscovinom@gmail.com>
2023-01-11 19:01:02 -0300

    filter unique trips

--- models/projeto_brt_predicao/aux_brt_registros_trip.sql
+++ models/projeto_brt_predicao/aux_brt_registros_trip.sql
@@ -1,4 +1,5 @@
 {# Filtra dados históricos de GPS e adiciona identificação de sentido e trip #}
+
 WITH
   gps AS (
   SELECT
@@ -19,15 +20,21 @@ WITH
     AND "2022-12-02"),
   trips AS (
   SELECT
-    trip_id,
-    trip_short_name,
-    direction_id,
-    timestamp_captura AS data_versao
-  FROM
-    `rj-smtr-dev.gtfs.trips`
+    * EXCEPT(rn)
+  FROM (
+    SELECT
+      trip_id,
+      trip_short_name,
+      direction_id,
+      timestamp_captura AS data_versao,
+      ROW_NUMBER() OVER (PARTITION BY trip_short_name, direction_id ORDER BY trip_id) AS rn
+    FROM
+      `rj-smtr-dev.gtfs.trips`
+    WHERE
+      timestamp_captura = "2022-12-27 10:46:00"
+      AND direction_id IS NOT NULL )
   WHERE
-    timestamp_captura = "2022-12-27 10:46:00"
-    AND direction_id IS NOT NULL) -- TODO: atualizar gtfs para incluir sentido da 52
+    rn = 1) -- TODO: atualizar gtfs para incluir sentido da 52
 SELECT
   g.*,
   t.trip_id
