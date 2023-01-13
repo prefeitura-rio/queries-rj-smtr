8c433220e0a3d5dde207b909a264d8a4cb8d9a34
Raphael Rivas <raphaelrivas@hotmail.com>
2023-01-13 16:37:15 -0300

    minor spelling fix

--- dev/run.py
+++ dev/run.py
@@ -1,7 +1,7 @@
 from utils import run_dbt_model
 import os
 
-# Veja os parâmetros disponíveis da função run_dbt_model em util.py
+# Veja os parâmetros disponíveis da função run_dbt_model em utils.py
 
 run_dbt_model(
     dataset_id="example",
