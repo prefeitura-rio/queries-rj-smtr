2b061b91b3d6ef8b0cdaeae5cbcb8208af9eb89e
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-02-09 19:37:25 -0300

    update variables in dbt_project

--- dev/run.py
+++ dev/run.py
@@ -1,9 +1,16 @@
 from utils import run_dbt_model
-import os
 
 # Veja os parâmetros disponíveis da função run_dbt_model em util.py
 
+# run_dbt_model(
+#     dataset_id="operacao",
+#     _vars={"end_date": "2023-01-31"}
+# )
+
 run_dbt_model(
-    dataset_id="example",
-    table_id="my_first_dbt_model",
+    dataset_id="projeto_subsidio_sppo",
+    table_id="viagem_completa",
+    upstream=True,
+    exclude="+gps_sppo",
+    _vars={"run_date": "2023-01-31"}
 )
