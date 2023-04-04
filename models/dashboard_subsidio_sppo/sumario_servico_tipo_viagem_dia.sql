046682eb530a8ca3e631c37f29dec61c283f191e
Fernanda Scovino <fscovinom@gmail.com>
2023-04-04 11:06:12 -0300

    add tabela do dash

---
+++ models/dashboard_subsidio_sppo/sumario_servico_tipo_viagem_dia.sql
@@ -0,0 +1,89 @@
+-- DECLARE
+--   "{{ var(""{{ var("DATA_SUBSIDIO_V2_INICIO") }}"") }}" STRING DEFAULT "{{ var(""{{ var("DATA_SUBSIDIO_V2_INICIO") }}"") }}";
+-- DECLARE
+--   "{{ var(""{{ var("DATA_SUBSIDIO_QUINZENA_ATUAL") }}"") }}" STRING DEFAULT "{{ var(""{{ var("DATA_SUBSIDIO_QUINZENA_ATUAL") }}"") }}";
+
+  -- Gabarito v1 + v2
+WITH
+  planejado AS (
+  SELECT
+    DISTINCT `data`,
+    tipo_dia,
+    consorcio,
+    servico
+  FROM
+    `rj-smtr`.`projeto_subsidio_sppo`.`viagem_planejada`
+  WHERE
+    `data` <= DATE( "{{ var(""{{ var("DATA_SUBSIDIO_QUINZENA_ATUAL") }}"") }}" )
+    AND distancia_total_planejada > 0 ),
+  sumario_v1 AS ( -- Viagens v1
+  SELECT
+    `data`,
+    servico,
+    "Não classificado" AS tipo_viagem,
+    viagens_subsidio AS viagens,
+    ROUND(distancia_total_subsidio, 2) AS km_apurada
+  FROM
+    `rj-smtr`.`dashboard_subsidio_sppo`.`sumario_dia`
+  WHERE
+    `data` < DATE( "{{ var(""{{ var("DATA_SUBSIDIO_V2_INICIO") }}"") }}" ) ),
+  tipo_viagem_v2 AS ( -- Classifica os tipos de viagem (v2)
+  SELECT
+    `data`,
+    id_veiculo,
+    status
+  FROM
+    `rj-smtr`.`veiculo`.`sppo_veiculo_dia`
+  WHERE
+    `data` BETWEEN DATE( "{{ var(""{{ var("DATA_SUBSIDIO_V2_INICIO") }}"") }}" )
+    AND DATE( "{{ var(""{{ var("DATA_SUBSIDIO_QUINZENA_ATUAL") }}"") }}" ) ),
+  viagem_v2 AS (
+  SELECT
+    `data`,
+    servico_realizado AS servico,
+    id_veiculo,
+    id_viagem,
+    distancia_planejada
+  FROM
+    `rj-smtr`.`projeto_subsidio_sppo`.`viagem_completa`
+  WHERE
+    `data` BETWEEN DATE( "{{ var(""{{ var("DATA_SUBSIDIO_V2_INICIO") }}"") }}" )
+    AND DATE( "{{ var(""{{ var("DATA_SUBSIDIO_QUINZENA_ATUAL") }}"") }}" ) ),
+  sumario_v2 AS (
+  SELECT
+    v.`data`,
+    v.servico,
+    ve.status AS tipo_viagem,
+    COUNT(id_viagem) AS viagens,
+    ROUND(SUM(distancia_planejada), 2) AS km_apurada
+  FROM
+    viagem_v2 v
+  LEFT JOIN
+    tipo_viagem_v2 ve
+  ON
+    ve.`data` = v.`data`
+    AND ve.id_veiculo = v.id_veiculo
+  GROUP BY
+    1,
+    2,
+    3 ) -- Todas AS viagens v1+ v2
+SELECT
+  p.`data`,
+  p.tipo_dia,
+  p.consorcio,
+  p.servico,
+  IFNULL(COALESCE(v1.tipo_viagem, v2.tipo_viagem), "Sem viagem apurada") AS tipo_viagem,
+  IFNULL(COALESCE(v1.viagens, v2.viagens), 0) AS viagens,
+  IFNULL(COALESCE(v1.km_apurada, v2.km_apurada), 0) AS km_apurada
+FROM
+  planejado p
+LEFT JOIN
+  sumario_v1 v1
+ON
+  p.`data` = v1.`data`
+  AND p.servico = v1.servico
+LEFT JOIN
+  sumario_v2 v2
+ON
+  p.`data` = v2.`data`
+  AND p.servico = v2.servico
