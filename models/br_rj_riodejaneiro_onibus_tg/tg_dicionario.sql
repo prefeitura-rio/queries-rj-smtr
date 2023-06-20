7adcdb0eef290e6e4c425dbfa4d902ce0783fe5b
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-06-20 01:39:16 -0300

    update dicionario

---
+++ models/br_rj_riodejaneiro_onibus_tg/tg_dicionario.sql
@@ -0,0 +1,12 @@
+{{ 
+  config(alias='dicionario') 
+}}
+
+SELECT
+  SAFE_CAST(chave AS STRING) AS chave,
+  SAFE_CAST(cobertura_temporal AS STRING) AS cobertura_temporal,
+  SAFE_CAST(id_tabela AS STRING) AS id_tabela,
+  SAFE_CAST(nome_coluna AS STRING) AS nome_coluna,
+  SAFE_CAST(valor AS STRING) AS valor
+FROM
+  {{var('tg_dicionario_staging')}}
