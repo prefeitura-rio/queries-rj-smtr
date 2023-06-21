fec8ca605c80b60a8978b0b5055a1fd6d25c0b9f
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-06-21 14:14:59 -0300

    update transacao structure

--- models/br_rj_riodejaneiro_onibus_tg/tg_transacao.sql
+++ models/br_rj_riodejaneiro_onibus_tg/tg_transacao.sql
@@ -8,28 +8,21 @@
 }}
 
 SELECT
-  SAFE_CAST(tipo_registro AS STRING) AS tipo_registro,
-  SAFE_CAST(versao_registro AS STRING) AS versao_registro,
-  SAFE_CAST(DATETIME(TIMESTAMP_ADD(TIMESTAMP(DATE_ADD(DATE '2002-12-31', INTERVAL data DAY)), INTERVAL hora SECOND), "America/Sao_Paulo") AS DATETIME) AS timestamp_transacao,
-  SAFE_CAST(emissor_aplicacao || "_" || aplicacao AS STRING) AS numero_aplicacao,
-  SAFE_CAST(tsn AS INT64) AS sequencial_transacao,
-  SAFE_CAST(status AS INT64) AS status,
+  SAFE_CAST(DATE_ADD(DATE '2002-12-31', INTERVAL data DAY) AS DATE) AS data,
+  SAFE_CAST(codigo_empresa AS STRING) AS id_empresa,
   SAFE_CAST(num_onibus AS STRING) AS id_veiculo,
-  SAFE_CAST(tipo_embarque AS INT64) AS tipo_embarque,
-  SAFE_CAST(SAFE_CAST(tipo_debito AS FLOAT64) AS INT64) AS tipo_debito,
-  SAFE_CAST(SAFE_CAST(mensagem_debito AS FLOAT64) AS INT64) AS mensagem_debito,
+  SAFE_CAST(uid AS STRING) AS id_cartao, 
+  SAFE_CAST(emissor_aplicacao || "_" || aplicacao AS STRING) AS id_tipo_cartao,
+  SAFE_CAST(tsn AS STRING) AS sequencial_transacao_veiculo,
+  SAFE_CAST(DATETIME(TIMESTAMP_ADD(TIMESTAMP(DATE_ADD(DATE '2002-12-31', INTERVAL data DAY)), INTERVAL hora SECOND), "America/Sao_Paulo") AS DATETIME) AS timestamp_transacao,
+  SAFE_CAST(tipo_embarque AS STRING) AS tipo_embarque,
+  SAFE_CAST(SAFE_CAST(tipo_debito AS FLOAT64) AS STRING) AS tipo_debito,
+  SAFE_CAST(SAFE_CAST(mensagem_debito AS FLOAT64) AS STRING) AS mensagem_debito,
   SAFE_CAST(valor_tarifa/100 AS FLOAT64) AS valor_tarifa,
   SAFE_CAST(valor_tarifa_anterior/100 AS FLOAT64) AS valor_tarifa_anterior,
   SAFE_CAST(valor_debitado/100 AS FLOAT64) AS valor_debitado,
-  SAFE_CAST(COALESCE(SAFE_CAST(diferenca_valor_debitado AS INT64)/100, 0) AS FLOAT64) AS diferenca_valor_debitado,
   SAFE_CAST(SAFE_CAST(valor_promo_desconto AS INT64)/100 AS FLOAT64) AS valor_promo_desconto,
-  SAFE_CAST(SAFE_CAST(valor_acumulado AS INT64)/100 AS FLOAT64) AS valor_acumulado,
-  SAFE_CAST(origin_file AS STRING) AS arquivo_origem,
-  SAFE_CAST(garagem AS STRING) AS garagem,
-  SAFE_CAST(file_date AS STRING) AS data_arquivo_origem,
-  SAFE_CAST(codigo_empresa AS INT64) AS codigo_empresa,
-  SAFE_CAST(uid AS STRING) AS id_cartao,
-  SAFE_CAST(DATE_ADD(DATE '2002-12-31', INTERVAL data DAY) AS DATE) AS data
+  SAFE_CAST(SAFE_CAST(valor_acumulado AS INT64)/100 AS FLOAT64) AS valor_total_integracao 
 FROM
   {{var('tg_transacao_staging')}}
 WHERE
