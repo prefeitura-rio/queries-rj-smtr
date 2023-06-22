866d6b5fb7247be3fa6ea0cafb9b98205895d8b3
Igor Laltuf <igorlaltuf@gmail.com>
2023-06-22 14:03:08 -0300

    Criação das tabelas do dash do CIMU

---
+++ models/dashboard_cimu/README.md
@@ -0,0 +1,29 @@
+# Dashboard CIMU
+* Versão: 0.1
+* Data de início: 20/06/2023
+* Autor: Igor Laltuf
+
+## Descrição
+Este repositório contém a documentação do conjunto de dados utilizados no dashboard do Centro Integrado de Mobilidade Urbana (CIMU).
+
+
+## Sobre os dados
+
+### 1 - Tabela sumario_servico_dia_2022
+
+Contém os dados dos subsídios desde o início da apuração até 15-01-2023.
+
+#### Tabelas de origem:
+rj-smtr.dashboard_subsidio_sppo.sumario_servico_dia_historico
+rj-smtr.dashboard_subsidio_sppo.sumario_servico_tipo_viagem_dia
+
+
+
+### 2 - Tabela sumario_servico_dia_2023TD
+
+Contém os dados dos subsídios a partir de 16-01-2023, excluindo o intervalo do carnaval de 17-02-2023 até 22-02-2023.
+
+#### Tabelas de origem:
+rj-smtr.dashboard_subsidio_sppo.sumario_servico_dia_historico
+rj-smtr.dashboard_subsidio_sppo.sumario_servico_tipo_viagem_dia
+
---
+++ models/dashboard_cimu/sumario_servico_dia_2022.sql
@@ -0,0 +1,38 @@
+/*  Query para criar a view com dados de 2022*/ 
+
+SELECT
+  s.`data`,
+  s.tipo_dia,
+  s.servico,
+  CAST(s.viagens AS INT) AS viagens,
+  ROUND(s.km_apurada, 2) AS km_apurada,
+  ROUND(s.km_planejada, 2) AS km_planejada,
+  ROUND(s.perc_km_planejada, 2) AS perc_km_planejada,
+  ROUND(s.valor_subsidio_pago, 2) AS valor_subsidio_pago,
+  t.consorcio  
+FROM
+  `rj-smtr.dashboard_subsidio_sppo.sumario_servico_dia_historico` s
+LEFT JOIN (
+  SELECT DISTINCT
+    data,
+    servico,
+    consorcio,
+    km_apurada
+  FROM
+    `rj-smtr.dashboard_subsidio_sppo.sumario_servico_tipo_viagem_dia`
+) t ON s.servico = t.servico AND s.`data` = t.`data`
+WHERE
+  s.`data` < '2023-01-16' 
+GROUP BY
+  s.`data`,
+  s.tipo_dia,
+  s.servico,
+  viagens,
+  km_apurada,
+  km_planejada,
+  perc_km_planejada,
+  valor_subsidio_pago,
+  t.consorcio
+ORDER BY
+  s.servico,
+  s.`data`
