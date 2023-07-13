f3859f1fc9377d1983b99cec496fcca8228757da
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-07-12 21:28:53 -0300

    update sppo_licenciamento params

--- models/veiculo/sppo_licenciamento.sql
+++ models/veiculo/sppo_licenciamento.sql
@@ -12,39 +12,17 @@ with
     stu as (
         select *
         from {{ ref("sppo_licenciamento_stu") }} as t
-        {% if var("stu_data_versao") != "" -%}
-        where data = date("{{ var('stu_data_versao') }}")
+        {% if var("stu_data_versao") != "" %}
+            where data = date("{{ var('stu_data_versao') }}")
         {% else %}
-        where data = date_add(date("{{ var('run_date') }}"), interval 5 day)
-        {%- endif %}
+            {% if execute %}
+                {% set licenciamento_date = run_query("SELECT MIN(data) FROM " ~ ref("sppo_licenciamento") ~ " WHERE data >= DATE_ADD(DATE('" ~ var("run_date") ~ "'), INTERVAL 5 DAY)").columns[0].values()[0] %}
+            {% endif %}
+
+            where data = DATE("{{ licenciamento_date }}")
+        {% endif %}
         AND tipo_veiculo NOT LIKE "%ROD%"
-    )--,
-    -- Solicitações válidas de licenciamento
-    -- TODO: add if run_date < ... antes de subir p proda te data max de validade das
-    -- solicitacoes
-    -- solicitacao as (
-    --     select * except (solicitacao)
-    --     from {{ ref("sppo_licenciamento_solicitacao") }} as t
-    --     where
-    --         data = date("{{ var('sppo_licenciamento_solicitacao_data_versao') }}")  -- fixo
-    --         and status = "Válido"
-    --         and solicitacao != "Baixa"
-    --         AND tipo_veiculo NOT LIKE "%ROD%"
-    -- )
--- select 
---     {% if var("stu_data_versao") != "" -%}
---     date("{{ var('stu_data_versao') }}") as data,
---     {% else %}
---     date_add(date("{{ var('run_date') }}"), interval 5 day) as data,
---     {%- endif %}
---     * except (data, timestamp_captura)
--- from solicitacao sol
--- union all
--- Se tiver id_veiculo em solicitacao e for valido, substitui o que esta em
--- licenciamento
--- (
-    select stu.* except (timestamp_captura)
-    from stu
-    -- left join solicitacao sol on stu.id_veiculo = sol.id_veiculo
-    -- where sol.id_veiculo is null
--- )
+    )
+select 
+    stu.* except (timestamp_captura)
+from stu
