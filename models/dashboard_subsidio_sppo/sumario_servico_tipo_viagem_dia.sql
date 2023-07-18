dd47f4cba798a19b13463b8daa21361cff3affdc
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-07-18 18:04:23 -0300

    Revert "update sumario_servico_tipo_viagem_dia"
    This reverts commit 3e5e056c67c2ba45b32bc4dd76d8acd85513a2ff.

--- models/dashboard_subsidio_sppo/sumario_servico_tipo_viagem_dia.sql
+++ models/dashboard_subsidio_sppo/sumario_servico_tipo_viagem_dia.sql
@@ -7,126 +7,10 @@ WITH
     consorcio,
     servico
   FROM
-    {{ ref("viagem_planejada") }} -- `rj-smtr.projeto_subsidio_sppo.viagem_planejada`
+    {{ ref("viagem_planejada") }}
   WHERE
     `data` <= DATE( "{{ var("end_date") }}" )
-    AND distancia_total_planejada IS NOT NULL ),
-  planejado_sumario_dia AS (
-  SELECT
-    consorcio,
-    DATA,
-    tipo_dia,
-    trip_id_planejado AS trip_id,
-    servico,
-    sentido,
-    CASE
-      WHEN sentido = "C" THEN MAX(distancia_planejada)
-    ELSE
-    SUM(distancia_planejada)
-  END
-    AS distancia_planejada,
-    MAX(distancia_total_planejada) AS distancia_total_planejada,
-    NULL AS viagens_planejadas
-  FROM
-    {{ ref("viagem_planejada") }} -- `rj-smtr.projeto_subsidio_sppo.viagem_planejada`
-  WHERE
-    DATA >= "2022-06-01"
-    AND DATA < DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" )
-    AND distancia_total_planejada > 0
-  GROUP BY
-    1,
-    2,
-    3,
-    4,
-    5,
-    6),
-  viagem_sumario_dia AS (
-  SELECT
-    DATA,
-    trip_id,
-    COUNT(id_viagem) AS viagens_realizadas
-  FROM
-    {{ ref("viagem_completa") }} -- `rj-smtr.projeto_subsidio_sppo.viagem_completa`
-  WHERE
-    DATA >= "2022-06-01"
-    AND DATA < DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" )
-  GROUP BY
-    1,
-    2),
-  previa_sumario_dia AS (
-  SELECT
-    consorcio,
-    DATA,
-    tipo_dia,
-    servico,
-    distancia_planejada,
-    NULL AS viagens_planejadas,
-    IFNULL(SUM(v.viagens_realizadas), 0) AS viagens_subsidio,
-    distancia_total_planejada,
-    (IFNULL(SUM(v.viagens_realizadas), 0) * distancia_planejada) AS distancia_total_subsidio
-  FROM
-    planejado_sumario_dia AS p
-  LEFT JOIN
-    viagem_sumario_dia AS v
-  USING
-    (trip_id,
-      DATA)
-  GROUP BY
-    1,
-    2,
-    3,
-    4,
-    5,
-    8 ),
-  sumario_dia_agg AS (
-  SELECT
-    consorcio,
-    DATA,
-    tipo_dia,
-    servico,
-    NULL AS viagens_planejadas,
-    IFNULL(SUM(viagens_subsidio), 0) AS viagens_subsidio,
-    distancia_total_planejada,
-    ROUND(SUM(distancia_total_subsidio), 3) AS distancia_total_subsidio
-  FROM
-    previa_sumario_dia
-  GROUP BY
-    1,
-    2,
-    3,
-    4,
-    5,
-    7 ),
-  valor_sumario_dia AS (
-  SELECT
-    s.*,
-    v.valor_subsidio_por_km,
-    ROUND(distancia_total_subsidio * v.valor_subsidio_por_km, 2) AS valor_total_aferido,
-  IF
-    (distancia_total_planejada = 0, NULL, ROUND(100*distancia_total_subsidio/distancia_total_planejada, 2)) AS perc_distancia_total_subsidio
-  FROM
-    sumario_dia_agg s
-  LEFT JOIN (
-    SELECT
-      *
-    FROM
-      {{ ref("subsidio_data_versao_efetiva") }} -- `rj-smtr.projeto_subsidio_sppo.subsidio_data_versao_efetiva`
-    WHERE
-      DATA >= "2022-06-01"
-      AND DATA < DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" )) AS v
-  ON
-    v.data = s.data ),
-  sumario_dia AS (
-  SELECT
-    *,
-    CASE
-      WHEN (perc_distancia_total_subsidio < 80) OR (perc_distancia_total_subsidio IS NULL) THEN 0
-    ELSE
-    valor_total_aferido
-  END
-    AS valor_total_subsidio
-  FROM
-    valor_sumario_dia),
+    AND distancia_total_planejada > 0 ),
   sumario_v1 AS ( -- Viagens v1
   SELECT
     `data`,
@@ -135,7 +19,7 @@ WITH
     viagens_subsidio AS viagens,
     ROUND(distancia_total_subsidio, 2) AS km_apurada
   FROM
-     sumario_dia
+     `rj-smtr`.`dashboard_subsidio_sppo`.`sumario_dia`
   WHERE
     `data` < DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" ) ),
   tipo_viagem_v2 AS ( -- Classifica os tipos de viagem (v2)
@@ -156,7 +40,7 @@ WITH
     id_viagem,
     distancia_planejada
   FROM
-    {{ ref("viagem_completa") }} -- `rj-smtr.projeto_subsidio_sppo.viagem_completa`
+    {{ ref("viagem_completa") }}
   WHERE
     `data` BETWEEN DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" )
     AND DATE( "{{ var("end_date") }}" ) ),
