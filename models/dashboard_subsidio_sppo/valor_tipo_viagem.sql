b6acfc6a21cd6b5796cc8d7c955986236b9fd58f
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-07-18 02:56:24 -0300

    initial commit

--- models/dashboard_subsidio_sppo/valor_tipo_viagem.sql
+++ models/dashboard_subsidio_sppo/valor_tipo_viagem.sql
@@ -7,18 +7,123 @@
     )
 }}
 
--- todo: 
---  1. adicionar start e end date
---  2. rever classificacao na sppo_veiculo_dia
-
--- DEC RIO N. 51940/2023
-with valor_km as (
-    select null as indicador_licenciado, null as indicador_ar_condicionado, null as indicador_autuacao, "Nao licenciado" as status, 0 as valor_km, date("2023-01-16") as data_inicio, date("2023-12-31") as data_fim
-    union all
-    select true as indicador_licenciado, false as indicador_ar_condicionado, null as indicador_autuacao, "Licenciado sem ar" as status, 1.97 as valor_km, date("2023-01-16") as data_inicio, date("2023-12-31") as data_fim
-    union all
-    select true as indicador_licenciado, true as indicador_ar_condicionado, null as indicador_autuacao, "Licenciado com ar e não autuado (023.II)" as status, 2.81 as valor_km, date("2023-01-16") as data_inicio, date("2023-12-31") as data_fim
-    union all
-    select true as indicador_licenciado, true as indicador_ar_condicionado, true as indicador_autuacao, "Licenciado com ar e autuado (023.II)" as status, 0 as valor_km, date("2023-01-16") as data_inicio, date("2023-12-31") as data_fim
-)
-select * from valor_km
+WITH
+  valor_tipo_viagem AS (
+  -- Esta primeira consulta é necessária para que o Big Query compreenda que o datatype dos indicadores é BOOL
+  SELECT
+    STRUCT ( FALSE AS indicador_licenciado,
+      FALSE AS indicador_ar_condicionado,
+      FALSE AS indicador_autuacao_ar_condicionado,
+      FALSE AS indicador_autuacao_seguranca,
+      FALSE AS indicador_autuacao_limpeza,
+      FALSE AS indicador_autuacao_equipamento,
+      FALSE AS indicador_sensor_temperatura,
+      FALSE AS indicador_validador_sbd ) AS indicadores,
+    "Teste" AS status,
+    0 AS valor_km,
+    DATE("2023-01-16") AS data_inicio,
+    DATE("2023-12-31") AS data_fim,
+    "DECRETO RIO 51889/2022" AS legislacao,
+    0 AS ordem
+  UNION ALL
+  SELECT
+    STRUCT ( NULL AS indicador_licenciado,
+      NULL AS indicador_ar_condicionado,
+      NULL AS indicador_autuacao_ar_condicionado,
+      NULL AS indicador_autuacao_seguranca,
+      NULL AS indicador_autuacao_limpeza,
+      NULL AS indicador_autuacao_equipamento,
+      NULL AS indicador_sensor_temperatura,
+      NULL AS indicador_validador_sbd ) AS indicadores,
+    "Não licenciado" AS status,
+    0 AS valor_km,
+    DATE("2023-01-16") AS data_inicio,
+    DATE("2023-12-31") AS data_fim,
+    "DECRETO RIO 51889/2022" AS legislacao,
+    1 AS ordem
+  UNION ALL
+  SELECT
+    STRUCT ( TRUE AS indicador_licenciado,
+      TRUE AS indicador_ar_condicionado,
+      TRUE AS indicador_autuacao_ar_condicionado,
+      NULL AS indicador_autuacao_seguranca,
+      NULL AS indicador_autuacao_limpeza,
+      NULL AS indicador_autuacao_equipamento,
+      NULL AS indicador_sensor_temperatura,
+      NULL AS indicador_validador_sbd ) AS indicadores,
+    "Autuado por ar inoperante" AS status,
+    0 AS valor_km,
+    DATE("2023-01-16") AS data_inicio,
+    DATE("2023-12-31") AS data_fim,
+    "DECRETO RIO 51940/2023" AS legislacao,
+    2 AS ordem
+  UNION ALL
+  SELECT
+    STRUCT ( TRUE AS indicador_licenciado,
+      NULL AS indicador_ar_condicionado,
+      NULL AS indicador_autuacao_ar_condicionado,
+      TRUE AS indicador_autuacao_seguranca,
+      NULL AS indicador_autuacao_limpeza,
+      NULL AS indicador_autuacao_equipamento,
+      NULL AS indicador_sensor_temperatura,
+      NULL AS indicador_validador_sbd ) AS indicadores,
+    "Autuado por segurança" AS status,
+    0 AS valor_km,
+    DATE("2023-07-04") AS data_inicio,
+    DATE("2023-12-31") AS data_fim,
+    "DECRETO RIO 52820/2023" AS legislacao,
+    3 AS ordem
+  UNION ALL
+  SELECT
+    STRUCT ( TRUE AS indicador_licenciado,
+      NULL AS indicador_ar_condicionado,
+      NULL AS indicador_autuacao_ar_condicionado,
+      NULL AS indicador_autuacao_seguranca,
+      TRUE AS indicador_autuacao_limpeza,
+      TRUE AS indicador_autuacao_equipamento,
+      NULL AS indicador_sensor_temperatura,
+      NULL AS indicador_validador_sbd ) AS indicadores,
+    "Autuado por limpeza/equipamento" AS status,
+    0 AS valor_km,
+    DATE("2023-07-04") AS data_inicio,
+    DATE("2023-12-31") AS data_fim,
+    "DECRETO RIO 52820/2023" AS legislacao,
+    4 AS ordem
+  UNION ALL
+  SELECT
+    STRUCT ( TRUE AS indicador_licenciado,
+      FALSE AS indicador_ar_condicionado,
+      NULL AS indicador_autuacao_ar_condicionado,
+      FALSE AS indicador_autuacao_seguranca,
+      NULL AS indicador_autuacao_limpeza,
+      NULL AS indicador_autuacao_equipamento,
+      NULL AS indicador_sensor_temperatura,
+      NULL AS indicador_validador_sbd ) AS indicadores,
+    "Sem ar e não autuado" AS status,
+    1.97 AS valor_km,
+    DATE("2023-01-16") AS data_inicio,
+    DATE("2023-12-31") AS data_fim,
+    "DECRETO RIO 51889/2022" AS legislacao,
+    5 AS ordem
+  UNION ALL
+  SELECT
+    STRUCT ( TRUE AS indicador_licenciado,
+      TRUE AS indicador_ar_condicionado,
+      FALSE AS indicador_autuacao_ar_condicionado,
+      FALSE AS indicador_autuacao_seguranca,
+      NULL AS indicador_autuacao_limpeza,
+      NULL AS indicador_autuacao_equipamento,
+      NULL AS indicador_sensor_temperatura,
+      NULL AS indicador_validador_sbd ) AS indicadores,
+    "Com ar e não autuado" AS status,
+    2.81 AS valor_km,
+    DATE("2023-01-16") AS data_inicio,
+    DATE("2023-12-31") AS data_fim,
+    "DECRETO RIO 51889/2022" AS legislacao,
+    6 AS ordem )
+SELECT
+  *
+FROM
+  valor_tipo_viagem
+WHERE
+  status != "Teste"
