ce607c918f83663b424b189b9c899cf79c75735c
Fernanda Scovino <fscovinom@gmail.com>
2023-07-18 18:12:00 -0300

    remove version

--- models/dashboard_subsidio_sppo/sumario_servico_dia.sql
+++ models/dashboard_subsidio_sppo/sumario_servico_dia.sql
@@ -101,7 +101,6 @@ SELECT
   s.*,
   IF(p.valor IS NULL, st.valor_subsidio_apurado, 0) AS valor_subsidio_pago,
   IFNULL(-p.valor, 0) AS valor_penalidade,
-  "{{ var("version") }}" as versao
 FROM
   servico_km s
 LEFT JOIN
