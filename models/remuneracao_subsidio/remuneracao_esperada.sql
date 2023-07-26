eb957b2e37c1393b51879d15ba972c8b155409d2
Igor Laltuf <igorlaltuf@gmail.com>
2023-07-26 19:04:23 -0300

    fix: ajustes na tabela de remuneração esperada

--- models/remuneracao_subsidio/remuneracao_esperada.sql
+++ models/remuneracao_subsidio/remuneracao_esperada.sql
@@ -1,33 +1,33 @@
-/*  Remuneração Esperada */ 
+/* Remuneração esperada*/ 
+
 WITH
-  parametros_subsidio AS (
-  SELECT data_inicio, data_fim, irk, status
-  FROM {{ source('remuneracao_subsidio', 'parametros') }} 
+  parametros AS (
+  SELECT DISTINCT data_inicio, data_fim, irk, status
+  FROM {{ source('dashboard_subsidio_sppo', 'subsidio_parametros') }}  
   ),
   subsidiado AS (
   SELECT
     DISTINCT DATA,
-    servico 
+    servico
   FROM
     {{ source('dashboard_subsidio_sppo', 'sumario_servico_dia_historico') }} 
   WHERE
     DATA BETWEEN DATE('{{ var('data_inicio_subsidio') }}') 
     AND DATE('{{ var('data_fim_subsidio') }}')
-    AND valor_subsidio_pago > 0
-    ),
+    AND valor_subsidio_pago > 0),
   recurso AS (
   SELECT
     DISTINCT data_viagem AS DATA,
     servico
   FROM
-    {{ source('recurso_subsidio_sppo', 'reprocessamento') }} 
+     {{ source('recurso_subsidio_sppo', 'reprocessamento') }} 
   WHERE
-    data_viagem BETWEEN DATE('{{ var('data_inicio_subsidio') }}')
+    data_viagem BETWEEN DATE('{{ var('data_inicio_subsidio') }}') 
     AND DATE('{{ var('data_fim_subsidio') }}')
   UNION ALL (
     SELECT
       DISTINCT data_viagem AS DATA,
-      servico,
+      servico
     FROM
       {{ source('recurso_subsidio_sppo', 'bloqueio_via') }}
     WHERE
@@ -41,43 +41,39 @@ WITH
     consorcio,
     servico,
     tipo_viagem,
-    km_apurada AS quilometragem, 
-    parametros.irk
+    km_apurada AS quilometragem
   FROM
-    {{ source('dashboard_subsidio_sppo', 'sumario_servico_tipo_viagem_dia') }} as km_tipo_viagem
-
-  JOIN 
-    parametros_subsidio as parametros
-    ON DATE(km_tipo_viagem.DATA) BETWEEN DATE(parametros.data_inicio) AND DATE(parametros.data_fim) 
-    AND km_tipo_viagem.tipo_viagem = parametros.status
+    {{ source('remuneracao_subsidio', 'sumario_servico_tipo_viagem_dia') }} as k
   WHERE
     DATA BETWEEN DATE('{{ var('data_inicio_subsidio') }}')
     AND DATE('{{ var('data_fim_subsidio') }}')
-  ),
- tabela_final AS (  
-   SELECT
-     DISTINCT s.data,
-     consorcio,
-     s.servico,
-     t.tipo_viagem,
-     t.quilometragem,
-     t.irk
-   FROM
-     subsidiado AS s
-   LEFT JOIN
-     km_tipo_viagem AS t
-   USING
-     (DATA,
-       servico)
-   LEFT JOIN
-     recurso AS r
-   USING
-     (DATA,
-       servico)
-   WHERE
-     r.data is null
-     and r.servico is null
-     and data NOT IN ( {{ var('datas_invalidas') }} )
-   )
-   SELECT *, ROUND((irk * quilometragem), 2) AS remuneracao_esperada 
-   FROM tabela_final
+    ),
+  todas_viagens AS (
+  SELECT
+    DISTINCT s.data,
+    consorcio,
+    s.servico,
+    t.tipo_viagem,
+    t.quilometragem
+  FROM
+    subsidiado AS s
+  LEFT JOIN
+    km_tipo_viagem AS t
+  USING
+    (DATA,
+      servico)
+  LEFT JOIN
+    recurso AS r
+  USING
+    (DATA,
+      servico)
+  WHERE
+    r.data IS NULL
+    AND r.servico IS NULL
+    AND DATA NOT IN ( {{ var('datas_invalidas') }} )
+      ) 
+    SELECT todas_viagens.*, parametros.irk, ROUND((parametros.irk * todas_viagens.quilometragem), 2) AS remuneracao_esperada 
+    FROM todas_viagens 
+    LEFT JOIN parametros
+    ON todas_viagens.DATA BETWEEN parametros.data_inicio AND parametros.data_fim
+    AND todas_viagens.tipo_viagem = parametros.status
