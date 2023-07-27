04677a6055b806a8766f6dfe8add194231405ec3
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-07-27 15:50:05 -0300

    fix sumario_servico_tipo_viagem_dia

--- models/dashboard_subsidio_sppo/sumario_servico_tipo_viagem_dia.sql
+++ models/dashboard_subsidio_sppo/sumario_servico_tipo_viagem_dia.sql
@@ -1,4 +1,3 @@
--- Gabarito v1 + v2
 WITH
   planejado AS (
   SELECT
@@ -7,11 +6,9 @@ WITH
     consorcio,
     servico
   FROM
-    {{ ref("viagem_planejada") }}
+    {{ ref("sumario_servico_dia_historico") }}
   WHERE
-    `data` <= DATE( "{{ var("end_date") }}" )
-    AND (distancia_total_planejada > 0
-    OR distancia_total_planejada IS NOT NULL) ),
+    `data` <= DATE( "{{ var("end_date") }}" )),
   sumario_v1 AS ( -- Viagens v1
   SELECT
     `data`,
@@ -21,7 +18,7 @@ WITH
     viagens,
     ROUND(km_apurada, 2) AS km_apurada
   FROM
-     {{ ref("sumario_servico_dia_historico") }}
+    {{ ref("sumario_servico_dia_historico") }}
   WHERE
     `data` < DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" ) ),
   tipo_viagem_v2 AS ( -- Classifica os tipos de viagem (v2)
@@ -31,7 +28,7 @@ WITH
     status,
     SAFE_CAST(JSON_VALUE(indicadores,"$.indicador_ar_condicionado") AS BOOL) AS indicador_ar_condicionado
   FROM
-      {{ ref("sppo_veiculo_dia") }} -- `rj-smtr`.`veiculo`.`sppo_veiculo_dia`
+    {{ ref("sppo_veiculo_dia") }} -- `rj-smtr`.`veiculo`.`sppo_veiculo_dia`
   WHERE
     `data` BETWEEN DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" )
     AND DATE( "{{ var("end_date") }}" ) ),
@@ -43,7 +40,7 @@ WITH
     id_viagem,
     distancia_planejada
   FROM
-    {{ ref("viagem_completa") }}
+    {{ ref("viagem_completa") }} --`rj-smtr`.`projeto_subsidio_sppo`.`viagem_completa`
   WHERE
     `data` BETWEEN DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" )
     AND DATE( "{{ var("end_date") }}" ) ),
@@ -51,7 +48,7 @@ WITH
   SELECT
     *
   FROM
-    {{ ref("subsidio_parametros") }}
+    {{ ref("subsidio_parametros") }} --`rj-smtr`.`dashboard_subsidio_sppo`.`subsidio_parametros`
   WHERE
     status != "Não classificado"
   ORDER BY
@@ -80,12 +77,12 @@ WITH
   FROM
     tabela_status_array),
   status_flat AS (
-      SELECT DISTINCT 
-        status_t, 
-        status 
-      FROM 
-        status_update, 
-        UNNEST(status_array) AS status_t),
+  SELECT
+    DISTINCT status_t,
+    status
+  FROM
+    status_update,
+    UNNEST(status_array) AS status_t),
   tipo_viagem_v2_atualizado AS (
   SELECT
     * EXCEPT(status),
@@ -94,7 +91,7 @@ WITH
     tipo_viagem_v2 AS k
   LEFT JOIN
     status_flat AS u
-  ON 
+  ON
     u.status_t = k.status),
   sumario_v2 AS (
   SELECT
@@ -115,39 +112,40 @@ WITH
     1,
     2,
     3,
-    4 ) -- Todas AS viagens v1+ v2
-(SELECT
-  p.`data`,
+    4 )
+(
+SELECT
+  v1.`data`,
   p.tipo_dia,
   p.consorcio,
-  p.servico,
-  IFNULL(v1.tipo_viagem, "Sem viagem apurada") AS tipo_viagem,
+  v1.servico,
+  COALESCE(v1.tipo_viagem, "Sem viagem apurada") AS tipo_viagem,
   SAFE_CAST(indicador_ar_condicionado AS BOOL) AS indicador_ar_condicionado,
-  IFNULL(v1.viagens, 0) AS viagens,
-  IFNULL(v1.km_apurada, 0) AS km_apurada
+  COALESCE(v1.viagens, 0) AS viagens,
+  COALESCE(v1.km_apurada, 0) AS km_apurada
 FROM
-  planejado p
-LEFT JOIN
   sumario_v1 v1
+INNER JOIN
+  planejado p
 ON
   p.`data` = v1.`data`
   AND p.servico = v1.servico
 WHERE
   p.`data` < DATE( "{{ var("DATA_SUBSIDIO_V2_INICIO") }}" ))
-UNION ALL
-(SELECT
-  p.`data`,
+UNION ALL (
+SELECT
+  v2.`data`,
   p.tipo_dia,
   p.consorcio,
-  p.servico,
-  IFNULL(v2.tipo_viagem, "Sem viagem apurada") AS tipo_viagem,
+  v2.servico,
+  COALESCE(v2.tipo_viagem, "Sem viagem apurada") AS tipo_viagem,
   v2.indicador_ar_condicionado,
-  IFNULL(v2.viagens, 0) AS viagens,
-  IFNULL(v2.km_apurada, 0) AS km_apurada
+  COALESCE(v2.viagens, 0) AS viagens,
+  COALESCE(v2.km_apurada, 0) AS km_apurada
 FROM
-  planejado p
-LEFT JOIN
   sumario_v2 v2
+INNER JOIN
+  planejado p
 ON
   p.`data` = v2.`data`
   AND p.servico = v2.servico
