5c72d49aa1eab8b3776d0aee237d5dbcadeee560
Fernanda Scovino <fscovinom@gmail.com>
2023-08-21 18:58:05 -0300

    hotfix: fill null values + add refs

--- models/dashboard_subsidio_sppo/sumario_servico_dia_tipo_sem_glosa.sql
+++ models/dashboard_subsidio_sppo/sumario_servico_dia_tipo_sem_glosa.sql
@@ -1,15 +1,17 @@
 -- TABELA TEMPORÁRIA até decisão judicial final 
 -- Valores válidos entre 16-01-2023 e 31-12-2023 (subsidio_km = 2.81)
 
-WITH
+-- TODO: Consertar parametrizacao para demais datas (separar parametros em subsidio x desconto)
+WITH 
 parametros AS (
- SELECT 
-  MAX(IF(status = 'Licenciado sem ar e não autuado', subsidio_km, NULL)) AS subsidio_km_sem_ar_n_autuado,
-  MAX(IF(status = 'Licenciado com ar e não autuado', subsidio_km, NULL)) AS subsidio_km_sem_glosa 
- FROM `rj-smtr.dashboard_subsidio_sppo.subsidio_parametros` -- não parametrizar (dbt pega dados de rj-smtr-dev)
- WHERE data_inicio >= '2023-07-04'
+  SELECT 
+    MAX(IF(status = 'Licenciado sem ar e não autuado', subsidio_km, NULL)) AS subsidio_km_sem_ar_n_autuado,
+    MAX(IF(status = 'Licenciado com ar e não autuado', subsidio_km, NULL)) AS subsidio_km_sem_glosa 
+  FROM 
+    {{ ref("subsidio_parametros") }}
+  WHERE
+    data_inicio >= '2023-07-04'
 )
-  
 SELECT
   consorcio,
   data,
@@ -18,7 +20,7 @@ SELECT
   viagens AS viagens_subsidio,
   km_planejada AS distancia_total_planejada,
   km_apurada AS distancia_total_subsidio,
-  0 AS valor_total_aferido,
+  null AS valor_total_aferido, -- TODO: Excluir essa coluna? é utilizada?
   perc_km_planejada AS perc_distancia_total_subsidio,
   -- Valor total sem glosas: quando existe subsidio (POD>80%), adiciona o valor glosado por tipo de viagem ao total
   CASE
@@ -27,19 +29,19 @@ SELECT
   0
 END
   AS valor_total_subsidio,
-  viagens_n_licenciado,
+  COALESCE(viagens_n_licenciado, 0) AS viagens_n_licenciado,
   COALESCE(km_apurada_n_licenciado, 0) AS km_apurada_n_licenciado,
-  viagens_autuado_ar_inoperante,
+  COALESCE(viagens_autuado_ar_inoperante, 0) AS viagens_autuado_ar_inoperante,
   COALESCE(km_apurada_autuado_ar_inoperante, 0) AS km_apurada_autuado_ar_inoperante,
-  viagens_autuado_seguranca,
+  COALESCE(viagens_autuado_seguranca, 0) AS viagens_autuado_seguranca,
   COALESCE(km_apurada_autuado_seguranca, 0) AS km_apurada_autuado_seguranca,
-  viagens_autuado_limpezaequipamento,
+  COALESCE(viagens_autuado_limpezaequipamento, 0) AS viagens_autuado_limpezaequipamento,
   COALESCE(km_apurada_autuado_limpezaequipamento, 0) AS km_apurada_autuado_limpezaequipamento,
-  viagens_licenciado_sem_ar_n_autuado,
+  COALESCE(viagens_licenciado_sem_ar_n_autuado, 0) AS viagens_licenciado_sem_ar_n_autuado,
   COALESCE(km_apurada_licenciado_sem_ar_n_autuado, 0) AS km_apurada_licenciado_sem_ar_n_autuado,
-  viagens_licenciado_com_ar_n_autuado,
+  COALESCE(viagens_licenciado_com_ar_n_autuado, 0) AS viagens_licenciado_com_ar_n_autuado,
   COALESCE(km_apurada_licenciado_com_ar_n_autuado, 0) AS km_apurada_licenciado_com_ar_n_autuado
 FROM
-  rj-smtr.dashboard_subsidio_sppo.sumario_servico_dia_tipo -- não parametrizar (dbt pega dados de rj-smtr-dev)
+  {{ ref("sumario_servico_dia_tipo") }}
 CROSS JOIN
   parametros
