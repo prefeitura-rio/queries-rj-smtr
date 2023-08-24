582ec93fccea3d0be72281cdcfeb3b65389b0de0
Fernanda Scovino <fscovinom@gmail.com>
2023-08-24 20:04:20 -0300

    initial commit

--- models/veiculo/sppo_veiculo_dia.sql
+++ models/veiculo/sppo_veiculo_dia.sql
@@ -7,7 +7,17 @@
     )
 }}
 
+
 WITH
+  gps AS (
+  SELECT
+    DISTINCT
+    data,
+    id_veiculo
+  FROM
+    {{ ref("gps_sppo") }}
+  WHERE
+    data = DATE("2023-08-02T01:00:00") ),
   licenciamento AS (
   SELECT
     DATE("{{ var('run_date') }}") AS data,
@@ -17,7 +27,8 @@ WITH
     indicador_ar_condicionado,
     TRUE AS indicador_licenciado
   FROM
-    {{ ref("sppo_licenciamento") }} --`rj-smtr`.`veiculo`.`sppo_licenciamento`
+    {{ ref("sppo_licenciamento") }}
+  -- TODO (para pensar depois): tem como simplificar? deixar mais explicito qual data é valida para veiculos licenciados, possivelmente refatorar o tratamento do licenciamento ou centralizar todas as infos numa tabela única de veiculo
   {%- if var("stu_data_versao") != "" %}
   WHERE 
     data = DATE("{{ var('stu_data_versao') }}")
@@ -28,47 +39,14 @@ WITH
   WHERE 
     data = DATE("{{ licenciamento_date }}")
   {% endif -%}  
+    and length(id_veiculo) = 6 -- exclui BRT
   ),
-  gps AS (
-  SELECT
-    DISTINCT data,
-    id_veiculo
-  FROM
-    {{ ref("gps_sppo") }}
-  WHERE
-    data = DATE("{{ var('run_date') }}") ),
   autuacoes AS (
   SELECT
-    DISTINCT data_infracao AS data,
+    data_infracao as data,
     placa,
-    id_infracao
-  FROM
-    {{ ref("sppo_infracao") }}
-  WHERE
-  {%- if execute %}
-    {% set infracao_date = run_query("SELECT MIN(data) FROM " ~ ref("sppo_infracao") ~ " WHERE data >= DATE_ADD(DATE('" ~ var("run_date") ~ "'), INTERVAL 7 DAY)").columns[0].values()[0] %}
-  {% endif -%}
-    data = DATE("{{ infracao_date }}")
-    AND data_infracao = DATE("{{ var('run_date') }}")
-    AND modo = "ONIBUS"),
-  autuacao_ar_condicionado AS (
-  SELECT
-    data,
-    placa,
-    TRUE AS indicador_autuacao_ar_condicionado
-  FROM
-    autuacoes
-  WHERE
-    id_infracao = "023.II" ),
-  autuacao_seguranca AS (
-  SELECT
-    data,
-    placa,
-    TRUE AS indicador_autuacao_seguranca
-  FROM
-    autuacoes
-  WHERE
-    id_infracao IN (
+    sum(case when id_infracao = "023.II" then 1 else 0 end) > 0 as indicador_autuacao_ar_condicionado,
+    sum(case when id_infracao in ( -- TODO: transformar a lista em parametro (var)
       "016.VI",
       "023.VII",
       "024.II",
@@ -86,16 +64,9 @@ WITH
       "025.XII",
       "025.XIII",
       "025.XIV",
-      "026.X") ),
-  autuacao_equipamento AS (
-  SELECT
-    data,
-    placa,
-    TRUE AS indicador_autuacao_equipamento
-  FROM
-    autuacoes
-  WHERE
-    id_infracao IN (
+      "026.X")
+ then 1 else 0 end ) > 0 as indicador_autuacao_seguranca,
+  sum(case when id_infracao in ( -- TODO: transformar a lista em parametro (var)
       "023.IV",
       "023.V",
       "023.VI",
@@ -112,74 +83,54 @@ WITH
       "025.VIII",
       "025.IX",
       "025.X",
-      "025.XI") ),
-  autuacao_limpeza AS (
-  SELECT
-    data,
-    placa,
-    TRUE AS indicador_autuacao_limpeza
+      "025.XI") then 1 else 0 end ) > 0 as indicador_autuacao_equipamento,
+      -- TODO: transformar a lista em parametro (var)
+      sum(case when id_infracao in ("023.IX", "024.X") then 1 else 0 end) > 0 as indicador_autuacao_limpeza,
   FROM
-    autuacoes
+    {{ ref("sppo_infracao") }}
+  -- TODO (para pensar depois): mesmo ponto do licenciamento
   WHERE
-    id_infracao IN (
-      "023.IX",
-      "024.X") ),
-  autuacoes_agg AS (
-  SELECT
-    DISTINCT *
-  FROM
-    autuacao_ar_condicionado
-  FULL JOIN
-    autuacao_seguranca
-  USING
-    (data,
-      placa)
-  FULL JOIN
-    autuacao_equipamento
-  USING
-    (data,
-      placa)
-  FULL JOIN
-    autuacao_limpeza
-  USING
-    (data,
-      placa) ),
-  gps_licenciamento_autuacao AS (
-  SELECT
-    data,
-    id_veiculo,
-    STRUCT( COALESCE(l.indicador_licenciado, FALSE) AS indicador_licenciado,
-      COALESCE(l.indicador_ar_condicionado, FALSE) AS indicador_ar_condicionado,
-      COALESCE(a.indicador_autuacao_ar_condicionado, FALSE) AS indicador_autuacao_ar_condicionado,
-      COALESCE(a.indicador_autuacao_seguranca, FALSE) AS indicador_autuacao_seguranca,
-      COALESCE(a.indicador_autuacao_limpeza, FALSE) AS indicador_autuacao_limpeza,
-      COALESCE(a.indicador_autuacao_equipamento, FALSE) AS indicador_autuacao_equipamento) AS indicadores
-  FROM
-    gps g
-  LEFT JOIN
-    licenciamento AS l
-  USING
-    (data,
-      id_veiculo)
-  LEFT JOIN
-    autuacoes_agg AS a
-  USING
-    (data,
-      placa))
+  {%- if execute %}
+    {% set infracao_date = run_query("SELECT MIN(data) FROM " ~ ref("sppo_infracao") ~ " WHERE data >= DATE_ADD(DATE('" ~ var("run_date") ~ "'), INTERVAL 7 DAY)").columns[0].values()[0] %}
+  {% endif -%}
+    data = DATE("{{ infracao_date }}")
+    AND data_infracao = DATE("{{ var('run_date') }}")
+    AND modo = "ONIBUS"
+  GROUP BY 
+    placa, data
+)
+-- TODO: solucionar coluna de status, ta vindo nula!
 SELECT
-  gla.* EXCEPT(indicadores),
-  TO_JSON(indicadores) AS indicadores,
-  status,
-  "{{ var("version") }}" AS versao
+  COALESCE(g.data, l.data) as data,
+  COALESCE(g.id_veiculo, l.id_veiculo) as id_veiculo,
+  TO_JSON(STRUCT( 
+    COALESCE(l.indicador_licenciado, FALSE) AS indicador_licenciado,
+    COALESCE(l.indicador_ar_condicionado, FALSE) AS indicador_ar_condicionado,
+    COALESCE(a.indicador_autuacao_ar_condicionado, FALSE) AS indicador_autuacao_ar_condicionado,
+    COALESCE(a.indicador_autuacao_seguranca, FALSE) AS indicador_autuacao_seguranca,
+    COALESCE(a.indicador_autuacao_limpeza, FALSE) AS indicador_autuacao_limpeza,
+    COALESCE(a.indicador_autuacao_equipamento, FALSE) AS indicador_autuacao_equipamento
+  )) AS indicadores,
+  p.status,
+  "" AS versao
 FROM
-  gps_licenciamento_autuacao AS gla
-LEFT JOIN
-  {{ ref("subsidio_parametros") }} AS p --`rj-smtr.dashboard_subsidio_sppo.subsidio_parametros` 
+  gps g
+FULL JOIN
+  licenciamento AS l
+USING
+  (id_veiculo, data)
+FULL OUTER JOIN
+  autuacoes AS a
 ON
-  gla.indicadores.indicador_licenciado = p.indicador_licenciado
-  AND gla.indicadores.indicador_ar_condicionado = p.indicador_ar_condicionado
-  AND gla.indicadores.indicador_autuacao_ar_condicionado = p.indicador_autuacao_ar_condicionado
-  AND gla.indicadores.indicador_autuacao_seguranca = p.indicador_autuacao_seguranca
-  AND gla.indicadores.indicador_autuacao_limpeza = p.indicador_autuacao_limpeza
-  AND gla.indicadores.indicador_autuacao_equipamento = p.indicador_autuacao_equipamento
-  AND (data BETWEEN p.data_inicio AND p.data_fim)
+  l.data = a.data
+  AND l.placa = a.placa
+LEFT JOIN
+  {{ ref("subsidio_parametros") }} AS p
+ON
+  g.data BETWEEN p.data_inicio AND p.data_fim
+  AND l.indicador_licenciado = p.indicador_licenciado
+  AND l.indicador_ar_condicionado = p.indicador_ar_condicionado
+  AND a.indicador_autuacao_ar_condicionado = p.indicador_autuacao_ar_condicionado
+  AND a.indicador_autuacao_seguranca = p.indicador_autuacao_seguranca
+  AND a.indicador_autuacao_limpeza = p.indicador_autuacao_limpeza
+  AND a.indicador_autuacao_equipamento = p.indicador_autuacao_equipamento
