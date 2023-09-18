789aadd8b9e4d1318ccaed383576e6d1e46c1629
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-09-18 17:32:32 -0300

    update tables

---
+++ models/br_rj_riodejaneiro_bilhetagem/staging_grupo.sql
@@ -0,0 +1,34 @@
+{{
+  config(
+    schema='br_rj_riodejaneiro_bilhetagem_staging',
+    alias='grupo',
+  )
+}}
+
+WITH 
+    linha AS (
+        SELECT
+            data,
+            timestamp_captura,
+            DATETIME(PARSE_TIMESTAMP('%a, %d %b %Y %T GMT', SAFE_CAST(JSON_VALUE(content, '$.DT_INCLUSAO') AS STRING)), "America/Sao_Paulo") AS dt_inclusao,
+            SAFE_CAST(JSON_VALUE(content, '$.CD_GRUPO') AS STRING) AS cd_grupo,
+            SAFE_CAST(JSON_VALUE(content, '$.CD_TIPO_GRUPO') AS STRING) AS cd_tipo_grupo,
+            SAFE_CAST(JSON_VALUE(content, '$.DS_GRUPO') AS STRING) AS ds_grupo,
+        FROM
+            {{ var("bilhetagem_linha_staging") }}
+    ),
+    linha_rn AS (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (PARTITION BY data, cd_linha) AS rn
+        FROM
+            linha
+        ORDER BY
+            data
+    )
+SELECT
+  * EXCEPT(rn)
+FROM
+  linha_rn
+WHERE
+  rn = 1
