44d2c9c939f5e5ffa27630dd9cebd65acf104feb
Rafael <rafael.c.pinheiro96@gmail.com>
2023-09-22 17:49:36 -0300

    add demonstrativo

--- models/br_rj_riodejaneiro_bilhetagem/demonstrativo_jae_dia.sql
+++ models/br_rj_riodejaneiro_bilhetagem/demonstrativo_jae_dia.sql
@@ -1,22 +1,30 @@
 -- Dado teste para comparação de valores
+WITH
+  transacao_agg AS (
+  SELECT
+    20001 AS agency_id, -- TODO: puxar do GTFS
+    data,
+    COUNT(*) AS n_transacoes,
+    MAX(valor_transacao) AS valor_bilhete,
+    SUM(valor_transacao) AS valor_bruto
+  FROM
+    {{ ref("transacao") }}
+  WHERE
+    data >= "2023-08-01"
+  GROUP BY
+    1,
+    2 )
 SELECT
-  20001 AS agency_id, -- TODO: puxar do GTFS
-  EXTRACT(DATE FROM datetime_transacao) AS data,
-  EXTRACT(DATE FROM datetime_processamento) AS datetime_processamento,
-  COUNT(*) AS n_transacoes,
-  MAX(valor_transacao) AS valor_bilhete,
-  ROUND(SUM(valor_transacao), 2) AS valor_bruto,
-  NULL AS tarifa_cbd,
-  NULL AS valor_liquido
+  *,
+  0.04 * valor_bruto AS tarifa_cbd,
+  valor_bruto - (0.04 * valor_bruto) AS valor_liquido_cct,
+  CASE
+    WHEN EXTRACT(DAYOFWEEK FROM data) IN (1, 7) THEN DATE_ADD(DATE_TRUNC(data, WEEK(MONDAY)), INTERVAL 7 DAY)
+  ELSE
+    DATE_ADD(data, INTERVAL 1 DAY)
+  END 
+  AS data_liquidacao -- TODO: adicionar feriados
 FROM
-  {{ ref("transacao") }}
-WHERE
-  datetime_transacao >= "2023-08-01"
-GROUP BY
-  1,
-  2,
-  3
-ORDER BY
-  1,
-  2,
-  3
+  transacao_agg t
+ORDER BY 
+  data
