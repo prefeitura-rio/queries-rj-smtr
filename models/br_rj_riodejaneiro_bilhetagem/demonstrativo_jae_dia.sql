2ac2dcdb719515f2edaf84d68469a78aa2351126
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-09-26 12:03:13 -0300

    change vars

--- models/br_rj_riodejaneiro_bilhetagem/demonstrativo_jae_dia.sql
+++ models/br_rj_riodejaneiro_bilhetagem/demonstrativo_jae_dia.sql
@@ -1,3 +1,8 @@
+{{
+  config(
+    materialized="view",
+  )
+}}
 -- Dado teste para comparação de valores
 WITH
   transacao_agg AS (
@@ -16,8 +21,8 @@ WITH
     2 )
 SELECT
   *,
-  0.04 * valor_bruto AS tarifa_cbd,
-  valor_bruto - (0.04 * valor_bruto) AS valor_liquido_cct,
+  {{ var("var_percentual_tarifa_cbd") }} * valor_bruto AS tarifa_cbd,
+  valor_bruto - ({{ var("var_percentual_tarifa_cbd") }} * valor_bruto) AS valor_liquido_cct,
   CASE
     WHEN EXTRACT(DAYOFWEEK FROM data) IN (1, 7) THEN DATE_ADD(DATE_TRUNC(data, WEEK(MONDAY)), INTERVAL 7 DAY)
   ELSE
@@ -26,5 +31,3 @@ SELECT
   AS data_liquidacao -- TODO: adicionar feriados
 FROM
   transacao_agg t
-ORDER BY 
-  data
