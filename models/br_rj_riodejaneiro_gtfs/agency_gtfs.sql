29df5b3b1f8d91a878394e3de0abfebab626ab0f
Carolina Gomes <gsv.lina@gmail.com>
2023-10-20 18:43:33 -0300

    alteração nas descrições

--- models/br_rj_riodejaneiro_gtfs/agency_gtfs.sql
+++ models/br_rj_riodejaneiro_gtfs/agency_gtfs.sql
@@ -7,8 +7,8 @@
 ) }} 
 
 
-SELECT SAFE_CAST(agency_id AS STRING) agency_id,
-  SAFE_CAST(data_versao AS DATE) data_versao,
+SELECT SAFE_CAST(data_versao AS DATE) data_versao,
+  SAFE_CAST(agency_id AS STRING) agency_id,
   SAFE_CAST(JSON_VALUE(content, '$.agency_name') AS STRING) agency_name,
   SAFE_CAST(JSON_VALUE(content, '$.agency_url') AS STRING) agency_url,
   SAFE_CAST(JSON_VALUE(content, '$.agency_timezone') AS STRING) agency_timezone,
