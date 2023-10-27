9b5381c60632288d95456db9ac136a2d65bd3ab6
Igor Laltuf <igorlaltuf@gmail.com>
2023-10-27 13:20:50 -0300

    remoção de macros

--- macros/filter_veiculo_datetime.sql
+++
@@ -1,7 +0,0 @@
-{% macro filter_veiculo_datetime(id_veiculo_amostra, datetime_partida_amostra, datetime_chegada_amostra) %}
-  {% set conditions = [] %}
-  {% for id_veiculo, datetime_partida, datetime_chegada in zip(id_veiculo_amostra, datetime_partida_amostra, datetime_chegada_amostra) %}
-    {% do conditions.append("OR (SUBSTRING(id_veiculo, 2) = '" ~ id_veiculo ~ "' AND timestamp_gps BETWEEN '" ~ datetime_partida ~ "' AND '" ~ datetime_chegada ~ "')") %}
-  {% endfor %}
-  {{ return(conditions | join(' ')) }}
-{% endmacro %}
--- macros/replace_servico.sql
+++
@@ -1,8 +0,0 @@
-{% macro replace_servico(id_veiculo_amostra, datetime_partida_amostra, datetime_chegada_amostra, servico_amostra) %}
-  {% for id_veiculo, datetime_partida, datetime_chegada, servico in zip(id_veiculo_amostra, datetime_partida_amostra, datetime_chegada_amostra, servico_amostra) %}
-    WHEN r.linha != 'GARAGEM' 
-    AND SUBSTRING(r.id_veiculo, 2) = '{{ id_veiculo }}'
-    AND r.timestamp_gps BETWEEN '{{ datetime_partida }}' AND '{{ datetime_chegada }}'
-    THEN '{{ servico }}'
-  {% endfor %}
-{% endmacro %}
