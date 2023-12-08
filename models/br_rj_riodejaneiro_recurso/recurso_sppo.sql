82a4d22074c66c0ef0dd4f49dbc790b363363b10
Carolina Gomes <gsv.lina@gmail.com>
2023-12-08 17:32:21 -0300

    ajusta datetime

--- models/br_rj_riodejaneiro_recurso/recurso_sppo.sql
+++ models/br_rj_riodejaneiro_recurso/recurso_sppo.sql
@@ -14,8 +14,8 @@ WITH recurso_sppo AS (
     JSON_EXTRACT_ARRAY(content, '$.customFieldValues') AS items,
     DATETIME(PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S', REGEXP_REPLACE(JSON_VALUE(content, '$.createdDate'), r'(\.\d+)?$', '')), 'America/Sao_Paulo') AS datetime_recurso,
     SAFE_CAST(protocol AS STRING) AS id_recurso,
-    --DATETIME(PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S%Ez', timestamp_captura), 'America/Sao_Paulo') AS datetime_captura
-    DATETIME(TIMESTAMP(timestamp_captura), "America/Sao_Paulo") AS datetime_captura
+    DATETIME(PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S%Ez', timestamp_captura), 'America/Sao_Paulo') AS datetime_captura
+    --DATETIME(TIMESTAMP(timestamp_captura), "America/Sao_Paulo") AS datetime_captura
   FROM 
     {{source('br_rj_riodejaneiro_recurso_staging', 
       'recurso_sppo')}} 
