fc45c942a240725ebf61e5b62ec4252a179aa9a8
Carolina Gomes <gsv.lina@gmail.com>
2023-12-20 18:47:24 -0300

    update de modelos

--- models/br_rj_riodejaneiro_recursos/recursos_sppo_viagens_individuais_ultimo_julgamento.sql
+++ models/br_rj_riodejaneiro_recursos/recursos_sppo_viagens_individuais_ultimo_julgamento.sql
@@ -1,38 +1,71 @@
 {{  config(
-    materialized = 'table',
+    materialized = 'incremental',
   )
 }}
-WITH expandida AS(
-  {{ 
-    abrir_tabela_recursos(
-     date_range_start=var('date_range_start'), 
-     date_range_end=var('date_range_end'), 
-     is_incremental=is_incremental()
-    ) 
-  }}
-),
-julgamento AS (
+WITH exploded AS (
   SELECT 
-    id_recurso, 
-    julgamento, 
-    motivo_julgamento, 
-    datetime_update,
-    LAG(julgamento) OVER (PARTITION BY id_recurso ORDER BY datetime_update) AS ultimo_julgamento
-  FROM
-    {{ ref('recursos_sppo_viagens_individuais_expandida') }}
-  WHERE 
-    julgamento IS NOT NULL
-)
+    id_recurso,
+    datetime_update AS data_julgamento, 
+    SAFE_CAST(COALESCE(JSON_VALUE(items, '$.value'), JSON_VALUE(items, '$.items[0].customFieldItem')) AS STRING
+    ) AS julgamento, 
+    SAFE_CAST(JSON_EXTRACT(items, '$.customFieldId') AS STRING ) AS field_id 
+  FROM 
+    {{ ref('staging_recursos_sppo_viagens_individuais') }}, 
+    UNNEST(items) items
+  {% if is_incremental() -%}
+    WHERE
+        DATE(data) BETWEEN DATE("{{var('date_range_start')}}") 
+        AND DATE("{{var('date_range_end')}}")
+        AND rn=1
+  {%- endif %}
+), 
+pivotado AS (
+  SELECT * EXCEPT(field_id)
+  FROM 
+    exploded 
+  WHERE field_id = '111865'
+
+), 
+{% if is_incremental() %}
+  julgamento AS (
+    SELECT 
+      p.id_recurso,
+      p.julgamento,
+      p.data_julgamento,
+      t.julgamento AS ultimo_julgamento    
+    FROM 
+      pivotado p
+    LEFT JOIN 
+      {{ this }} AS t
+    USING (id_recurso)
+  )
+{% else %}
+  julgamento AS (
+    SELECT 
+      id_recurso, 
+      julgamento, 
+      data_julgamento,
+      LAG(julgamento) OVER (PARTITION BY id_recurso ORDER BY data_julgamento) AS ultimo_julgamento
+    FROM
+      {% if is_incremental() %}
+        incremental 
+      {% else %}
+        pivotado 
+      {% endif %}
+    WHERE 
+      julgamento IS NOT NULL
+  )
+{% endif %}
+
 SELECT 
-  * EXCEPT(rn)
+  id_recurso, 
+  data_julgamento, 
+  julgamento
 FROM 
 (
   SELECT 
-    ROW_NUMBER() OVER(PARTITION BY j.id_recurso ORDER BY j.datetime_update DESC) AS rn,
-    j.id_recurso, 
-    j.julgamento, 
-    j.motivo_julgamento, 
-    j.datetime_update AS data_julgamento
+    ROW_NUMBER() OVER(PARTITION BY j.id_recurso ORDER BY j.data_julgamento DESC) AS rn,
+    *
   FROM 
     julgamento j
   WHERE 
