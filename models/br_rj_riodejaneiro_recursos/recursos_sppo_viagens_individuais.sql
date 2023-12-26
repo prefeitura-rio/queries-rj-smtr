f91e7b2af6dc7a255e08f74bbd5eeb5a39692aac
Carolina Gomes <gsv.lina@gmail.com>
2023-12-26 11:57:48 -0300

    recria alterações em uma nova barnch

--- models/br_rj_riodejaneiro_recursos/recursos_sppo_viagens_individuais.sql
+++ models/br_rj_riodejaneiro_recursos/recursos_sppo_viagens_individuais.sql
@@ -4,43 +4,27 @@
     'data_type' :'date',
     'granularity': 'day' },
       unique_key = 'id_recurso',
-      alias = 'recursos_sppo_viagens_individuais',
 ) }}
 
-
-WITH recurso_sppo AS (
-  SELECT
-    ROW_NUMBER() OVER(PARTITION BY protocol ORDER BY timestamp_captura DESC) AS rn,    
-    JSON_EXTRACT_ARRAY(content, '$.customFieldValues') AS items,
-    DATETIME(PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S', REGEXP_REPLACE(JSON_VALUE(content, '$.createdDate'), r'(\.\d+)?$', '')), 'America/Sao_Paulo') AS datetime_recurso,
-    SAFE_CAST(protocol AS STRING) AS id_recurso,
-    DATETIME(PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S%Ez', timestamp_captura), 'America/Sao_Paulo') AS datetime_captura
-
-  FROM 
-    {{source('br_rj_riodejaneiro_recursos_staging', 
-      'recursos_sppo_viagens_individuais')}} 
-  {% if is_incremental() -%}
-    WHERE
-        DATE(data) BETWEEN DATE("{{var('date_range_start')}}") 
-        AND DATE("{{var('date_range_end')}}")
-        AND DATETIME(PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S%Ez', timestamp_captura), 
-        "America/Sao_Paulo") BETWEEN DATETIME("{{var('date_range_start')}}") 
-        AND DATETIME("{{var('date_range_end')}}")
-  {%- endif %}
-), 
-exploded AS (
+WITH exploded AS (
   SELECT 
     id_recurso,
     datetime_recurso,
-    datetime_captura, 
+    datetime_captura,
+    datetime_update, 
     SAFE_CAST(COALESCE(JSON_VALUE(items, '$.value'), JSON_VALUE(items, '$.items[0].customFieldItem')) AS STRING
     ) AS value, 
     SAFE_CAST(JSON_EXTRACT(items, '$.customFieldId') AS STRING ) AS field_id 
   FROM 
-    recurso_sppo, 
+    {{ ref('staging_recursos_sppo_viagens_individuais') }}, 
     UNNEST(items) items
-  WHERE
-    rn = 1
+  
+ {% if is_incremental() -%}
+    WHERE
+        DATE(data) BETWEEN DATE("{{var('date_range_start')}}") 
+        AND DATE("{{var('date_range_end')}}")
+        AND rn=1
+  {%- endif %}
 ), 
 pivotado AS (
   SELECT *
@@ -56,38 +40,39 @@ pivotado AS (
 ), 
 tratado AS (
   SELECT 
-  id_recurso, 
-  datetime_captura, 
-  datetime_recurso,
-  SAFE_CAST(p.111865 AS STRING) AS julgamento, 
-  SAFE_CAST(p.111870 AS STRING) AS consorcio,
-  CASE
-    WHEN SAFE_CAST(p.111872 AS STRING) = "SR - Regular" THEN SAFE_CAST(p.111871 AS STRING)
-    ELSE CONCAT(REPLACE(SPLIT(SAFE_CAST(p.111872 AS STRING), "-")[OFFSET(0)], " ", ""), SAFE_CAST(p.111871 AS STRING))
-  END AS servico, 
-  SAFE_CAST(p.111873 AS STRING) AS id_veiculo, 
-  CASE
-      WHEN SAFE_CAST(p.111901 AS STRING) = "Ida" THEN "I"
-      WHEN SAFE_CAST(p.111901 AS STRING) = "Volta" THEN "V"
-      WHEN SAFE_CAST(p.111901 AS STRING) = "Circular" THEN "C"
-  END
-    AS sentido,
-  PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez',SAFE_CAST(p.111867 AS STRING), 'America/Sao_Paulo') AS data_viagem, 
-  PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', SAFE_CAST(p.111868 AS STRING), 'America/Sao_Paulo') AS hora_inicio_viagem, 
-  PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', SAFE_CAST(p.111869 AS STRING), 'America/Sao_Paulo') AS hora_fim_viagem, 
-  SAFE_CAST(p.111866 AS STRING) AS motivo, 
-  COALESCE(SAFE_CAST(p.111904 AS STRING), SAFE_CAST(p.111900 AS STRING)) AS motivo_julgamento, 
-  SAFE_CAST(p.125615 AS STRING) AS observacao,
+    id_recurso, 
+    datetime_captura, 
+    datetime_recurso,
+    datetime_update,
+    SAFE_CAST(p.111865 AS STRING) AS julgamento, 
+    SAFE_CAST(p.111870 AS STRING) AS consorcio,
+    CASE
+      WHEN SAFE_CAST(p.111872 AS STRING) = "SR - Regular" THEN SAFE_CAST(p.111871 AS STRING)
+      ELSE CONCAT(REPLACE(SPLIT(SAFE_CAST(p.111872 AS STRING), "-")[OFFSET(0)], " ", ""), SAFE_CAST(p.111871 AS STRING))
+    END AS servico, 
+    SAFE_CAST(p.111873 AS STRING) AS id_veiculo, 
+    CASE
+        WHEN SAFE_CAST(p.111901 AS STRING) = "Ida" THEN "I"
+        WHEN SAFE_CAST(p.111901 AS STRING) = "Volta" THEN "V"
+        WHEN SAFE_CAST(p.111901 AS STRING) = "Circular" THEN "C"
+    END
+      AS sentido,
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez',SAFE_CAST(p.111867 AS STRING), 'America/Sao_Paulo') AS data_viagem, 
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', SAFE_CAST(p.111868 AS STRING), 'America/Sao_Paulo') AS hora_inicio_viagem, 
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', SAFE_CAST(p.111869 AS STRING), 'America/Sao_Paulo') AS hora_fim_viagem, 
+    SAFE_CAST(p.111866 AS STRING) AS motivo, 
+    COALESCE(SAFE_CAST(p.111904 AS STRING), SAFE_CAST(p.111900 AS STRING)) AS motivo_julgamento, 
+    SAFE_CAST(p.125615 AS STRING) AS observacao,
    
-FROM 
-  pivotado p
+  FROM 
+    pivotado p
 )
-
 SELECT
     t.id_recurso,
     DATE(datetime_recurso) AS data,
     t.datetime_captura,
     t.datetime_recurso,
+    t.datetime_update,
     t.consorcio,
     t.servico,
     t.sentido,
@@ -107,8 +92,12 @@ SELECT
     t.motivo AS motivo_recurso,
     t.julgamento,
     t.motivo_julgamento,
-    t.observacao AS observacao_julgamento
-
-  
+    t.observacao AS observacao_julgamento,
+    j.data_julgamento
+ 
 FROM
     tratado t
+LEFT JOIN 
+   {{ ref('recursos_sppo_viagens_individuais_ultimo_julgamento') }} AS j
+
+  ON t.id_recurso = j.id_recurso
