19002dc152d11e52f30b9045a6453cf0df59a766
Carolina Gomes <gsv.lina@gmail.com>
2024-01-19 10:55:56 -0300

    update de modelos para teste

--- models/br_rj_riodejaneiro_recursos/recursos_sppo_viagens_individuais.sql
+++ models/br_rj_riodejaneiro_recursos/recursos_sppo_viagens_individuais.sql
@@ -8,7 +8,6 @@
 
 WITH exploded AS (
   SELECT
-    ROW_NUMBER() OVER(PARTITION BY id_recurso ORDER BY datetime_captura DESC) AS rn, 
     id_recurso,
     datetime_recurso,
     datetime_captura,
@@ -24,11 +23,13 @@ WITH exploded AS (
     WHERE
       DATE(data) BETWEEN DATE("{{var('date_range_start')}}") 
         AND DATE("{{var('date_range_end')}}")
+      AND rn=1
   {%- endif %}
 
 ),  
 pivotado AS (
-  SELECT *
+  SELECT *,
+  ROW_NUMBER() OVER(PARTITION BY id_recurso ORDER BY datetime_captura DESC) AS rn,
   FROM 
     exploded PIVOT(
       ANY_VALUE(value) FOR field_id IN (
@@ -38,10 +39,9 @@ pivotado AS (
         '111900'
       )
     )
-  WHERE rn = 1
 ), 
 tratado AS (
-  SELECT 
+  SELECT
     id_recurso, 
     datetime_captura, 
     datetime_recurso,
@@ -68,6 +68,7 @@ tratado AS (
    
   FROM 
     pivotado p
+  WHERE rn=1
 )
 SELECT
       t.id_recurso,
@@ -103,6 +104,4 @@ LEFT JOIN
     {{ ref('recursos_sppo_viagens_individuais_ultimo_julgamento') }} AS j
 
   ON t.id_recurso = j.id_recurso
-
-
 
