153f2aec3681b98ed93c6bfbc64ff5e4853148a3
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2024-01-23 11:15:46 -0300

    commit inicial

--- models/veiculo/sppo_registro_agente_verao.sql
+++ models/veiculo/sppo_registro_agente_verao.sql
@@ -1,15 +1,15 @@
-
- {{ config(
-       materialized='incremental',
-       partition_by={
-              "field":"data",
-              "data_type": "date",
-              "granularity":"day"
-       },
-       unique_key="id_registro",
-       incremental_strategy="merge",
-       merge_exclude_columns = ["timestamp_captura"],
-)
+{{ 
+  config(
+    materialized="incremental",
+    partition_by={
+      "field":"data",
+      "data_type": "date",
+      "granularity":"day"
+    },
+    unique_key="id_registro",
+    incremental_strategy="merge",
+    merge_update_columns=["data", "datetime_registro", "id_registro", "id_veiculo", "servico", "link_foto", "validacao"],
+  )
 }}
 
 SELECT
