8bfa88825f0c3412d55f6fb7ed2ce99f5b8d9291
Rodrigo Cunha <66736583+eng-rodrigocunha@users.noreply.github.com>
2024-01-25 19:04:10 -0300

    Hotfix sumario dia tipo (#200)
    * commit inicial
    
    * altera sumario

--- models/dashboard_subsidio_sppo/sumario_servico_dia_tipo_sem_glosa.sql
+++ models/dashboard_subsidio_sppo/sumario_servico_dia_tipo_sem_glosa.sql
@@ -1,31 +1,29 @@
--- TABELA TEMPORÁRIA até decisão judicial final 
--- Valores válidos entre 16-01-2023 e 31-12-2023 (subsidio_km = 2.81)
-
--- TODO: Consertar parametrizacao para demais datas (separar parametros em subsidio x desconto)
-WITH 
-parametros AS (
-  SELECT 
-    MAX(IF(status = 'Licenciado sem ar e não autuado', subsidio_km, NULL)) AS subsidio_km_sem_ar_n_autuado,
-    MAX(IF(status = 'Licenciado com ar e não autuado', subsidio_km, NULL)) AS subsidio_km_sem_glosa 
-  FROM 
+WITH
+  parametros AS (
+  SELECT
+    data_inicio,
+    data_fim,
+    MAX(IF(status = "Licenciado sem ar e não autuado", subsidio_km, NULL)) AS subsidio_km_sem_ar_n_autuado,
+    MAX(IF(status = "Licenciado com ar e não autuado", subsidio_km, NULL)) AS subsidio_km_sem_glosa
+  FROM
     {{ ref("subsidio_parametros") }}
   WHERE
-    data_inicio >= '2023-07-04'
-    AND data_fim <= '2023-12-31'
-)
+    data_inicio >= "2023-07-04"
+  GROUP BY
+    1,
+    2 )
 SELECT
   consorcio,
-  data,
+  DATA,
   tipo_dia,
   servico,
   viagens AS viagens_subsidio,
   km_planejada AS distancia_total_planejada,
   km_apurada AS distancia_total_subsidio,
-  null AS valor_total_aferido, -- TODO: Excluir essa coluna? é utilizada?
-  perc_km_planejada AS perc_distancia_total_subsidio,
-  -- Valor total sem glosas: quando existe subsidio (POD>80%), adiciona o valor glosado por tipo de viagem ao total
+  NULL AS valor_total_aferido, -- TODO: Excluir essa coluna? é utilizada? perc_km_planejada AS perc_distancia_total_subsidio,
+  -- Valor total sem glosas: quando existe subsidio (POD>80%),  adiciona o valor glosado por tipo de viagem ao total
   CASE
-    WHEN perc_km_planejada >= 80 THEN ROUND(COALESCE(valor_subsidio_pago, 0) + COALESCE(km_apurada_autuado_ar_inoperante * subsidio_km_sem_glosa, 0) + COALESCE(km_apurada_autuado_seguranca * subsidio_km_sem_glosa, 0) + COALESCE(km_apurada_autuado_limpezaequipamento * subsidio_km_sem_glosa, 0) + COALESCE(km_apurada_licenciado_sem_ar_n_autuado * (subsidio_km_sem_glosa - subsidio_km_sem_ar_n_autuado), 0), 2)
+    WHEN perc_km_planejada >= 80 THEN ROUND(COALESCE(valor_subsidio_pago, 0) + COALESCE(km_apurada_registrado_com_ar_inoperante * subsidio_km_sem_glosa, 0) + COALESCE(km_apurada_autuado_ar_inoperante * subsidio_km_sem_glosa, 0) + COALESCE(km_apurada_autuado_seguranca * subsidio_km_sem_glosa, 0) + COALESCE(km_apurada_autuado_limpezaequipamento * subsidio_km_sem_glosa, 0) + COALESCE(km_apurada_licenciado_sem_ar_n_autuado * (subsidio_km_sem_glosa - subsidio_km_sem_ar_n_autuado), 0), 2)
   ELSE
   0
 END
@@ -41,8 +39,13 @@ END
   COALESCE(viagens_licenciado_sem_ar_n_autuado, 0) AS viagens_licenciado_sem_ar_n_autuado,
   COALESCE(km_apurada_licenciado_sem_ar_n_autuado, 0) AS km_apurada_licenciado_sem_ar_n_autuado,
   COALESCE(viagens_licenciado_com_ar_n_autuado, 0) AS viagens_licenciado_com_ar_n_autuado,
-  COALESCE(km_apurada_licenciado_com_ar_n_autuado, 0) AS km_apurada_licenciado_com_ar_n_autuado
+  COALESCE(km_apurada_licenciado_com_ar_n_autuado, 0) AS km_apurada_licenciado_com_ar_n_autuado,
+  COALESCE(viagens_registrado_com_ar_inoperante, 0) AS viagens_registrado_com_ar_inoperante,
+  COALESCE(km_apurada_registrado_com_ar_inoperante, 0) AS km_apurada_registrado_com_ar_inoperante,
 FROM
   {{ ref("sumario_servico_dia_tipo") }}
-CROSS JOIN
+LEFT JOIN
   parametros
+ON
+  DATA BETWEEN data_inicio
+  AND data_fim
