397f5ebe29c5b8c739819b223b186afd0645e0e2
Rafael <rafael.c.pinheiro96@gmail.com>
2024-02-01 18:23:13 -0300

    cria tabela staging gratuidade

--- models/br_rj_riodejaneiro_bilhetagem_staging/staging_gratuidade.sql
+++ models/br_rj_riodejaneiro_bilhetagem_staging/staging_gratuidade.sql
@@ -4,27 +4,15 @@
   )
 }}
 
-WITH gratuidade AS (
-  SELECT
+SELECT
     data,
-        SAFE_CAST(id AS STRING) AS id,
-        timestamp_captura,
-        SAFE_CAST(JSON_VALUE(content, '$.cd_cliente') AS STRING) AS cd_cliente,
-        DATETIME(PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', SAFE_CAST(JSON_VALUE(content, '$.data_inclusao') AS STRING)), 'America/Sao_Paulo') AS data_inclusao,
-        SAFE_CAST(JSON_VALUE(content, '$.id_status_gratuidade') AS STRING) AS id_status_gratuidade,
-        SAFE_CAST(JSON_VALUE(content, '$.id_tipo_gratuidade') AS STRING) AS id_tipo_gratuidade
-  FROM
-    {{ source('br_rj_riodejaneiro_bilhetagem_staging', 'gratuidade') }}
-)
-SELECT 
-  * EXCEPT(rn)
+    SAFE_CAST(id AS STRING) AS id,
+    DATETIME(PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S%Ez', timestamp_captura), "America/Sao_Paulo") AS timestamp_captura,
+    SAFE_CAST(JSON_VALUE(content, '$.cd_cliente') AS STRING) AS cd_cliente,
+    DATETIME(PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', SAFE_CAST(JSON_VALUE(content, '$.data_inclusao') AS STRING)), 'America/Sao_Paulo') AS data_inclusao,
+    SAFE_CAST(JSON_VALUE(content, '$.id_status_gratuidade') AS STRING) AS id_status_gratuidade,
+    SAFE_CAST(JSON_VALUE(content, '$.id_tipo_gratuidade') AS STRING) AS id_tipo_gratuidade,
+    SAFE_CAST(JSON_VALUE(content, '$.tipo_gratuidade') AS STRING) AS tipo_gratuidade
 FROM
-(
-  SELECT
-    *,
-    ROW_NUMBER() OVER (PARTITION BY id ORDER BY timestamp_captura DESC) AS rn
-  FROM
-    gratuidade
-)
-WHERE
-  rn = 1
+  {{ source('br_rj_riodejaneiro_bilhetagem_staging', 'gratuidade') }}
+
