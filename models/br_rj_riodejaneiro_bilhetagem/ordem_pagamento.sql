59dd9e9d5be2c74e286c0188c3bd6ac26764e2ec
Rafael <rafael.c.pinheiro96@gmail.com>
2024-02-29 09:27:01 -0300

    ajuste filtro incremental

--- models/br_rj_riodejaneiro_bilhetagem/ordem_pagamento.sql
+++ models/br_rj_riodejaneiro_bilhetagem/ordem_pagamento.sql
@@ -10,49 +10,49 @@
   )
 }}
 
-WITH servico_motorista AS (
-    SELECT
-        * EXCEPT(rn)
-    FROM
-    (
-        SELECT
-            id_servico,
-            dt_fechamento,
-            nr_logico_midia,
-            cd_linha,
-            cd_operadora,
-            ROW_NUMBER() OVER (PARTITION BY id_servico, nr_logico_midia ORDER BY timestamp_captura DESC) AS rn
-        FROM
-            {{ ref("staging_servico_motorista") }}
-        {% if is_incremental() %}
-            WHERE
-                DATE(data) BETWEEN DATE_SUB(DATE("{{var('date_range_start')}}"), INTERVAL 1 DAY) AND DATE("{{var('date_range_end')}}")
-        {% endif %}  
-    )   
-),
-transacao AS (
+-- WITH servico_motorista AS (
+--     SELECT
+--         * EXCEPT(rn)
+--     FROM
+--     (
+--         SELECT
+--             id_servico,
+--             dt_fechamento,
+--             nr_logico_midia,
+--             cd_linha,
+--             cd_operadora,
+--             ROW_NUMBER() OVER (PARTITION BY id_servico, nr_logico_midia ORDER BY timestamp_captura DESC) AS rn
+--         FROM
+--             {{ ref("staging_servico_motorista") }}
+--         {% if is_incremental() %}
+--             WHERE
+--                 DATE(data) BETWEEN DATE_SUB(DATE("{{var('date_range_start')}}"), INTERVAL 1 DAY) AND DATE("{{var('date_range_end')}}")
+--         {% endif %}  
+--     )   
+-- ),
+WITH transacao AS (
     SELECT
         t.id,
         t.timestamp_captura,
-        DATE(data_processamento) AS data_processamento,
-        data_processamento AS datetime_processamento,
+        DATE(t.data_processamento) AS data_processamento,
+        t.data_processamento AS datetime_processamento,
         t.cd_linha,
         t.cd_operadora,
         t.valor_transacao,
         t.tipo_transacao,
         t.id_tipo_modal,
         t.cd_consorcio,
-        sm.dt_fechamento AS datetime_fechamento_servico,
-        sm.cd_linha AS cd_linha_servico,
-        sm.cd_operadora AS cd_operadora_servico,
+        -- sm.dt_fechamento AS datetime_fechamento_servico,
+        -- sm.cd_linha AS cd_linha_servico,
+        -- sm.cd_operadora AS cd_operadora_servico,
         t.id_servico
     FROM
         {{ ref("staging_transacao") }} t
-    LEFT JOIN
-        servico_motorista sm
-    ON
-        sm.id_servico = t.id_servico
-        AND sm.nr_logico_midia = t.nr_logico_midia_operador
+    -- LEFT JOIN
+    --     servico_motorista sm
+    -- ON
+    --     sm.id_servico = t.id_servico
+    --     AND sm.nr_logico_midia = t.nr_logico_midia_operador
     WHERE
         {% if is_incremental() %}
             DATE(t.data) BETWEEN DATE_SUB(DATE("{{var('date_range_start')}}"), INTERVAL 1 DAY) AND DATE_ADD(DATE("{{var('date_range_end')}}"), INTERVAL 1 DAY)
@@ -92,7 +92,7 @@ transacao_agg AS (
     WHERE
         -- Remove dados com data de ordem de pagamento maiores que a execução do modelo
         {% if is_incremental() %}
-            t.data_ordem DATE("{{var('date_range_end')}}")
+            t.data_ordem <= DATE("{{var('date_range_end')}}")
         {% else %}
             t.data_ordem <= CURRENT_DATE("America/Sao_Paulo")
         {% endif %}
