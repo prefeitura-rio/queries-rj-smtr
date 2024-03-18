fd1f36381c74c9690d96e47da0f4e8658aa91ebb
vtr363 <victor.m.rocha.vr@hotmail.com>
2024-03-18 15:59:56 -0300

    Ajusta tabelas do gtfs para adicionar feed_end_date

--- models/gtfs/stops_gtfs2.sql
+++ models/gtfs/stops_gtfs2.sql
@@ -7,27 +7,37 @@
 )}} 
 
 
-SELECT SAFE_CAST(data_versao AS DATE) as feed_start_date,
-    SAFE_CAST(stop_id AS STRING) stop_id,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_code') AS STRING) stop_code,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_name') AS STRING) stop_name,
-    SAFE_CAST(JSON_VALUE(content, '$.tts_stop_name') AS STRING) tts_stop_name,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_desc') AS STRING) stop_desc,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_lat') AS FLOAT64) stop_lat,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_lon') AS FLOAT64) stop_lon,
-    SAFE_CAST(JSON_VALUE(content, '$.zone_id') AS STRING) zone_id,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_url') AS STRING) stop_url,
-    SAFE_CAST(SAFE_CAST(SAFE_CAST(JSON_VALUE(content, '$.location_type') AS FLOAT64) AS INT64) AS STRING) location_type,
-    SAFE_CAST(JSON_VALUE(content, '$.parent_station') AS STRING) parent_station,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_timezone') AS STRING) stop_timezone,
-    SAFE_CAST(JSON_VALUE(content, '$.wheelchair_boarding') AS STRING) wheelchair_boarding,
-    SAFE_CAST(JSON_VALUE(content, '$.level_id') AS STRING) level_id,
-    SAFE_CAST(JSON_VALUE(content, '$.platform_code') AS STRING) platform_code,
-    '{{ var("version") }}' as versao_modelo
-FROM {{ source(
-            'br_rj_riodejaneiro_gtfs_staging',
-            'stops'
-        ) }}
-  {% if is_incremental() -%}
-    WHERE data_versao = '{{ var("data_versao_gtfs") }}'
-  {%- endif %}
+SELECT 
+    fi.feed_version,
+    SAFE_CAST(s.data_versao AS DATE) as feed_start_date,
+    fi.feed_end_date,
+    SAFE_CAST(s.stop_id AS STRING) stop_id,
+    SAFE_CAST(JSON_VALUE(s.content, '$.stop_code') AS STRING) stop_code,
+    SAFE_CAST(JSON_VALUE(s.content, '$.stop_name') AS STRING) stop_name,
+    SAFE_CAST(JSON_VALUE(s.content, '$.tts_stop_name') AS STRING) tts_stop_name,
+    SAFE_CAST(JSON_VALUE(s.content, '$.stop_desc') AS STRING) stop_desc,
+    SAFE_CAST(JSON_VALUE(s.content, '$.stop_lat') AS FLOAT64) stop_lat,
+    SAFE_CAST(JSON_VALUE(s.content, '$.stop_lon') AS FLOAT64) stop_lon,
+    SAFE_CAST(JSON_VALUE(s.content, '$.zone_id') AS STRING) zone_id,
+    SAFE_CAST(JSON_VALUE(s.content, '$.stop_url') AS STRING) stop_url,
+    SAFE_CAST(SAFE_CAST(SAFE_CAST(JSON_VALUE(s.content, '$.location_type') AS FLOAT64) AS INT64) AS STRING) location_type,
+    SAFE_CAST(JSON_VALUE(s.content, '$.parent_station') AS STRING) parent_station,
+    SAFE_CAST(JSON_VALUE(s.content, '$.stop_timezone') AS STRING) stop_timezone,
+    SAFE_CAST(JSON_VALUE(s.content, '$.wheelchair_boarding') AS STRING) wheelchair_boarding,
+    SAFE_CAST(JSON_VALUE(s.content, '$.level_id') AS STRING) level_id,
+    SAFE_CAST(JSON_VALUE(s.content, '$.platform_code') AS STRING) platform_code,
+    '{{ var("version") }}' AS versao_modelo
+FROM 
+    {{ source(
+        'br_rj_riodejaneiro_gtfs_staging',
+        'stops'
+    ) }} s
+JOIN
+    {{ ref('feed_info_gtfs2') }} fi 
+ON 
+    s.data_versao = fi.feed_start_date
+{% if is_incremental() -%}
+    WHERE 
+        s.data_versao = '{{ var("data_versao_gtfs") }}'
+        AND fi.feed_start_date = '{{ var("data_versao_gtfs") }}'
+{%- endif %}
