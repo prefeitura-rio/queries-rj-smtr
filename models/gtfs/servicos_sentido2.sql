6adfd73ef65f27c1ae366a6bef38a91c5e3292f1
Victor Miguel Rocha <Victor.m.rocha.vr@hotmail.com>
2024-03-19 11:44:11 -0300

    [WIP] Ajusta tabelas do gtfs para adicionar feed_end_date (#230)
    * Ajusta tabelas do gtfs para adicionar feed_end_date
    
    * Ajusta tipos de variaveis
    
    * Ajusta descrição das colunas

--- models/gtfs/servicos_sentido2.sql
+++ models/gtfs/servicos_sentido2.sql
@@ -4,63 +4,63 @@
 
 WITH
   servicos_exclusivos_sabado AS (
-  SELECT
-    DISTINCT servico
-  FROM
-    {{ ref("ordem_servico_gtfs2") }}
-  WHERE
-    tipo_dia = "Dia Útil"
-    AND viagens_planejadas = 0),
+    SELECT
+      DISTINCT servico
+    FROM
+      {{ ref("ordem_servico_gtfs2") }}
+    WHERE
+      tipo_dia = "Dia Útil"
+      AND viagens_planejadas = 0),
   servicos AS (
-  SELECT
-    * EXCEPT(versao_modelo,
-      shape)
-  FROM
-    {{ ref("trips_gtfs2") }} AS t
-  LEFT JOIN
-    {{ ref("shapes_geom_gtfs2") }} AS s
-  USING
-    (feed_start_date,
-      shape_id)
-  WHERE
-    (feed_start_date >= "2023-06-01" AND
-      ( trip_short_name NOT IN (SELECT * FROM servicos_exclusivos_sabado)
-        AND (service_id LIKE "U_R%"
-          OR service_id LIKE "U_O%") )
-      OR ( trip_short_name IN (SELECT * FROM servicos_exclusivos_sabado)
-        AND (service_id LIKE "S_R%"
-          OR service_id LIKE "S_O%")))
-    OR
-    (feed_start_date < "2023-06-01" AND
-      ( trip_short_name NOT IN (SELECT * FROM servicos_exclusivos_sabado)
-      OR ( trip_short_name IN (SELECT * FROM servicos_exclusivos_sabado)
-        AND service_id = "S")))
-    AND shape_distance IS NOT NULL),
+    SELECT
+      * EXCEPT(versao_modelo,
+        shape)
+    FROM
+      {{ ref("trips_gtfs2") }} AS t
+    LEFT JOIN
+      {{ ref("shapes_geom_gtfs2") }} AS s
+    USING
+      (feed_start_date,
+        shape_id)
+    WHERE
+      (feed_start_date >= "2023-06-01" AND
+        ( trip_short_name NOT IN (SELECT * FROM servicos_exclusivos_sabado)
+          AND (service_id LIKE "U_R%"
+            OR service_id LIKE "U_O%") )
+        OR ( trip_short_name IN (SELECT * FROM servicos_exclusivos_sabado)
+          AND (service_id LIKE "S_R%"
+            OR service_id LIKE "S_O%")))
+      OR
+      (feed_start_date < "2023-06-01" AND
+        ( trip_short_name NOT IN (SELECT * FROM servicos_exclusivos_sabado)
+        OR ( trip_short_name IN (SELECT * FROM servicos_exclusivos_sabado)
+          AND service_id = "S")))
+      AND shape_distance IS NOT NULL),
   servicos_rn AS (
-  SELECT
-    *,
-    ROW_NUMBER() OVER (PARTITION BY feed_start_date, trip_short_name, direction_id ORDER BY trip_short_name, service_id, shape_id, direction_id) AS rn
-  FROM
-    servicos ),
-  servicos_filtrada AS (
-  SELECT
-    * EXCEPT(rn)
-  FROM
-    servicos_rn
-  WHERE
-    rn = 1),
+    SELECT
+      *,
+      ROW_NUMBER() OVER (PARTITION BY feed_start_date, trip_short_name, direction_id ORDER BY trip_short_name, service_id, shape_id, direction_id) AS rn
+    FROM
+      servicos ),
+    servicos_filtrada AS (
+    SELECT
+      * EXCEPT(rn)
+    FROM
+      servicos_rn
+    WHERE
+      rn = 1),
   servicos_potencialmente_circulares AS (
-  SELECT
-    feed_start_date,
-    trip_short_name,
-    COUNT(DISTINCT direction_id) AS q_direcoes
-  FROM
-    servicos_filtrada
-  GROUP BY
-    1,
-    2
-  HAVING
-    COUNT(DISTINCT direction_id) = 1 )
+    SELECT
+      feed_start_date,
+      trip_short_name,
+      COUNT(DISTINCT direction_id) AS q_direcoes
+    FROM
+      servicos_filtrada
+    GROUP BY
+      1,
+      2
+    HAVING
+      COUNT(DISTINCT direction_id) = 1 )
 SELECT
   feed_start_date,
   trip_short_name AS servico,
--- models/gtfs/stop_times_gtfs2.sql
+++ models/gtfs/stop_times_gtfs2.sql
@@ -7,24 +7,34 @@
 )}} 
 
 
-SELECT SAFE_CAST(data_versao AS DATE) as feed_start_date,
-    SAFE_CAST(trip_id AS STRING) trip_id,
-    SAFE_CAST(JSON_VALUE(content, '$.arrival_time') AS STRING) arrival_time,
-    SAFE_CAST(JSON_VALUE(content, '$.departure_time') AS DATETIME) departure_time,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_id') AS STRING) stop_id,
-    SAFE_CAST(stop_sequence AS INT64) stop_sequence,
-    SAFE_CAST(JSON_VALUE(content, '$.stop_headsign') AS STRING) stop_headsign,
-    SAFE_CAST(JSON_VALUE(content, '$.pickup_type') AS STRING) pickup_type,
-    SAFE_CAST(JSON_VALUE(content, '$.drop_off_type') AS STRING) drop_off_type,
-    SAFE_CAST(JSON_VALUE(content, '$.continuous_pickup') AS STRING) continuous_pickup,
-    SAFE_CAST(JSON_VALUE(content, '$.continuous_drop_off') AS STRING) continuous_drop_off,
-    SAFE_CAST(JSON_VALUE(content, '$.shape_dist_traveled') AS FLOAT64) shape_dist_traveled,
-    SAFE_CAST(JSON_VALUE(content, '$.timepoint') AS STRING) timepoint,
-    '{{ var("version") }}' as versao_modelo
-FROM {{ source(
-            'br_rj_riodejaneiro_gtfs_staging',
-            'stop_times'
-        ) }}
-  {% if is_incremental() -%}
-    WHERE data_versao = '{{ var("data_versao_gtfs") }}'
-  {%- endif %}
+SELECT 
+    fi.feed_version,
+    SAFE_CAST(st.data_versao AS DATE) as feed_start_date,
+    fi.feed_end_date,
+    SAFE_CAST(st.trip_id AS STRING) trip_id,
+    SAFE_CAST(JSON_VALUE(st.content, '$.arrival_time') AS STRING) arrival_time,
+    SAFE_CAST(JSON_VALUE(st.content, '$.departure_time') AS DATETIME) departure_time,
+    SAFE_CAST(JSON_VALUE(st.content, '$.stop_id') AS STRING) stop_id,
+    SAFE_CAST(st.stop_sequence AS INT64) stop_sequence,
+    SAFE_CAST(JSON_VALUE(st.content, '$.stop_headsign') AS STRING) stop_headsign,
+    SAFE_CAST(JSON_VALUE(st.content, '$.pickup_type') AS STRING) pickup_type,
+    SAFE_CAST(JSON_VALUE(st.content, '$.drop_off_type') AS STRING) drop_off_type,
+    SAFE_CAST(JSON_VALUE(st.content, '$.continuous_pickup') AS STRING) continuous_pickup,
+    SAFE_CAST(JSON_VALUE(st.content, '$.continuous_drop_off') AS STRING) continuous_drop_off,
+    SAFE_CAST(JSON_VALUE(st.content, '$.shape_dist_traveled') AS FLOAT64) shape_dist_traveled,
+    SAFE_CAST(JSON_VALUE(st.content, '$.timepoint') AS STRING) timepoint,
+    '{{ var("version") }}' AS versao_modelo
+FROM 
+    {{ source(
+        'br_rj_riodejaneiro_gtfs_staging',
+        'stop_times'
+    ) }} st
+JOIN
+    {{ ref('feed_info_gtfs2') }} fi 
+ON 
+    st.data_versao = CAST(fi.feed_start_date AS STRING)
+{% if is_incremental() -%}
+    WHERE 
+        st.data_versao = '{{ var("data_versao_gtfs") }}'
+        AND fi.feed_start_date = '{{ var("data_versao_gtfs") }}'
+{%- endif %}
