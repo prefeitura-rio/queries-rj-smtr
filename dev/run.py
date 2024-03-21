9a992f99ef10ba09d54247db182f6b23a28e307f
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-03-21 12:37:12 -0300

    update run.py

--- dev/run.py
+++ dev/run.py
@@ -4,7 +4,6 @@ import os
 # Veja os parâmetros disponíveis da função run_dbt_model em util.py
 
 run_dbt_model(
-    dataset_id="projeto_subsidio_sppo",
-    table_id="subsidio_data_versao_efetiva",
-    _vars={"run_date": "2024-03-18"}
+    dataset_id="gtfs",
+    flags="--full-refresh",
 )
