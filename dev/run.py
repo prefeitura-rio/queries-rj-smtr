1d8ba4c5dce441697ffccd94d92d646667335d87
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-03-21 12:40:00 -0300

    remove run.py

--- dev/run.py
+++
@@ -1,9 +0,0 @@
-from utils import run_dbt_model
-import os
-
-# Veja os parâmetros disponíveis da função run_dbt_model em util.py
-
-run_dbt_model(
-    dataset_id="gtfs",
-    flags="--full-refresh",
-)
