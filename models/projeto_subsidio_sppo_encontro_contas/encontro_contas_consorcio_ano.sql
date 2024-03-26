efd2fe51993127e32543072f3d9d611b3e654b74
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-03-25 21:13:50 -0300

    refatora tabelas

--- models/projeto_subsidio_sppo_encontro_contas/encontro_contas_consorcio_ano.sql
+++ models/projeto_subsidio_sppo_encontro_contas/encontro_contas_consorcio_ano.sql
@@ -3,15 +3,18 @@ SELECT
   FROM
     DATA) AS ano,
   consorcio,
-  ROUND(SUM(km)/POW(10,6), 2) km_milhoes,
+  ROUND(SUM(km_apurada)/POW(10,6), 2) km_apurada_milhoes,
+  ROUND(SUM(km_planejada)/POW(10,6), 2) km_planejada_milhoes,
+  ROUND(SUM(km_subsidiada)/POW(10,6), 2) km_subsidiada_milhoes,
   ROUND(SUM(remuneracao_tarifaria)/POW(10,6), 2) remuneracao_tarifaria_milhoes,
   ROUND(SUM(subsidio)/POW(10,6), 2) subsidio_milhoes,
   ROUND(SUM(receita_aferida)/POW(10,6), 2) AS receita_aferida_milhoes,
   ROUND(SUM(receita_esperada)/POW(10,6), 2) AS receita_esperada_milhoes,
   ROUND(SUM(desconto)/POW(10,6), 2) AS desconto_milhoes,
-  ROUND(SUM(saldo)/POW(10, 6), 2) AS saldo_milhoes
+  ROUND(SUM(saldo)/POW(10, 6), 2) AS saldo_milhoes,
+  ROUND(SUM(quantidade_passageiros_total)/POW(10, 6), 2) AS quantidade_passageiros_total_milhoes,
 FROM
-  {{ ref("encontro_contas_consorcio_dia_cenario_2") }}
+  {{ ref("encontro_contas_consorcio_dia") }}
 GROUP BY
   1,
   2
