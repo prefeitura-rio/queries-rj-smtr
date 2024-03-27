22ded43a09269d6a613595425b6d192490a2fca3
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-03-26 21:08:11 -0300

    refatora sppo_licenciamento_v2

--- models/veiculo/sppo_licenciamento_v2.sql
+++ models/veiculo/sppo_licenciamento_v2.sql
@@ -1,75 +1,81 @@
 {{
     config(
         materialized="incremental",
-        unique_key="id_veiculo",
+        unique_key="id",
         incremental_strategy="merge",
     )
 }}
 
+/*
+    Validar
+        - unique_key
+            - id_veiculo = "A41062" (aparentemente foi baixado e depois o número foi reutilizado)
+        - merge_update_columns
+            - merge_update_columns=["data_ultima_vistoria", "versao", "datetime_ultima_atualizacao", "data_fim_vinculo"]
+            - Atualizar sempre versao e datetime_ultima_atualizacao sem atualizar as demais informações?
+        
+*/
+
 {%- if var("run_date") >= "2024-03-01" %}
-    {% if execute %}
+    {%- if execute %}
         {% set licenciamento_max_date = run_query("SELECT SAFE_CAST(MAX(DATA) AS DATE) FROM " ~ ref('sppo_licenciamento_stu') ~ " WHERE DATA >= DATE_SUB('" ~ var('run_date') ~ "', INTERVAL 5 DAY)").columns[0].values()[0] %}
-    {% endif %}
+    {% endif -%}
 
     WITH
     raw AS (
         SELECT
-            *,
-            "{{ var('version') }}" AS versao
+            id_veiculo || "_" || placa AS id,
+            *
         FROM
-            {{ ref("sppo_licenciamento_stu") }}
+        (
+            SELECT
+                * EXCEPT(data),
+                data
+            FROM
+                {{ ref("sppo_licenciamento_stu") }}
+            {%- if is_incremental() %}
+            WHERE
+                DATA >= DATE("{{ var('run_date') }}")
+            {% endif -%}
+            {%- if var("run_date") >= "2024-03-01" and var("run_date") <= "2024-03-15" %}
+            UNION ALL
+            SELECT
+                * EXCEPT(data),
+                DATE_ADD(DATE("{{ licenciamento_max_date }}"), INTERVAL 1 DAY) AS data
+            FROM
+                {{ ref("aux_sppo_licenciamento_stu") }}
+            WHERE
+                DATA = "2024-03-24"
+            {% endif -%}
+        )
         WHERE
-            DATA >= DATE("{{ var('run_date') }}")
+            tipo_veiculo NOT LIKE "%ROD%"
+            AND tipo_veiculo NOT LIKE "%BRT%"
     ),
-    treated AS (
+    treated_rn AS (
         SELECT
-            DATA,
-            modo,
-            id_veiculo,
-            SAFE_CAST(PARSE_DATETIME("%d/%m/%Y", data_inicio_vinculo) AS DATE) AS data_inicio_vinculo,
-            ano_fabricacao,
-            carroceria,
-            CASE
-                WHEN data_ultima_vistoria = "" THEN NULL
-            ELSE
-                SAFE_CAST(PARSE_DATETIME("%d/%m/%Y", data_ultima_vistoria) AS DATE)
-            END AS data_ultima_vistoria,
-            id_carroceria,
-            id_chassi,
-            id_fabricante_chassi,
-            id_interno_carroceria,
-            id_planta,
-            indicador_ar_condicionado,
-            indicador_elevador,
-            indicador_usb,
-            indicador_wifi,
-            nome_chassi,
-            permissao,
-            placa,
-            quantidade_lotacao_pe,
-            quantidade_lotacao_sentado,
-            tipo_combustivel,
-            tipo_veiculo,
-            status,
-            timestamp_captura,
-            versao,
-            ROW_NUMBER() OVER (PARTITION BY id_veiculo ORDER BY DATA DESC) rn
+            *,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY data DESC) rn
         FROM
             raw
     )
     SELECT
-        modo,
+        id,
         id_veiculo,
+        placa,
+        permissao,
+        modo,      
         data_inicio_vinculo,
         CASE
-            WHEN DATA != DATE("{{ licenciamento_max_date }}") THEN DATA
+            WHEN DATA < DATE("{{ licenciamento_max_date }}") THEN DATA
         ELSE
             NULL
         END AS data_fim_vinculo,
+        data_ultima_vistoria,
         ano_fabricacao,
         carroceria,
-        data_ultima_vistoria,
         id_carroceria,
+        nome_chassi,
         id_chassi,
         id_fabricante_chassi,
         id_interno_carroceria,
@@ -78,18 +84,16 @@
         indicador_elevador,
         indicador_usb,
         indicador_wifi,
-        nome_chassi,
-        permissao,
-        placa,
         quantidade_lotacao_pe,
         quantidade_lotacao_sentado,
         tipo_combustivel,
         tipo_veiculo,
         status,
         timestamp_captura,
-        versao,
+        "{{ var('version') }}" AS versao,
+        CURRENT_DATETIME("America/Sao_Paulo") AS datetime_ultima_atualizacao,
     FROM
-        treated
+        treated_rn
     WHERE
         rn = 1
 {%- else -%}
