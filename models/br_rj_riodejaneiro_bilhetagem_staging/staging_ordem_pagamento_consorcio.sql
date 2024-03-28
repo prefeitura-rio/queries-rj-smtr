7d9929e597817c492e4d265511a48e7087bf6f29
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2024-03-28 19:35:04 -0300

    Bilhetagem - Cria tabelas agregadas de ordem de pagamento da Jaé  (#237)
    cria tabelas ordem pagamento

--- models/br_rj_riodejaneiro_bilhetagem_staging/staging_ordem_pagamento_consorcio.sql
+++ models/br_rj_riodejaneiro_bilhetagem_staging/staging_ordem_pagamento_consorcio.sql
@@ -1,18 +1,18 @@
 {{
   config(
-    alias='ordem_pagamento',
+    alias='ordem_pagamento_consorcio',
   )
 }}
 
-WITH ordem_pagamento AS (
+WITH ordem_pagamento_consorcio AS (
   SELECT
     data,
-    SAFE_CAST(id AS STRING) AS id_ordem_pagamento,
+    SAFE_CAST(id AS STRING) AS id_ordem_pagamento_consorcio,
     timestamp_captura,
     DATETIME(PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', SAFE_CAST(JSON_VALUE(content, '$.data_inclusao') AS STRING)), "America/Sao_Paulo") AS datetime_inclusao,
     PARSE_DATE('%Y-%m-%d', SAFE_CAST(JSON_VALUE(content, '$.data_ordem') AS STRING)) AS data_ordem,
-    PARSE_DATE('%Y-%m-%d', SAFE_CAST(JSON_VALUE(content, '$.data_pagamento') AS STRING)) AS data_pagamento,
-    SAFE_CAST(JSON_VALUE(content, '$.id_status_ordem') AS STRING) AS id_status_ordem,
+    SAFE_CAST(JSON_VALUE(content, '$.id_consorcio') AS STRING) AS id_consorcio,
+    SAFE_CAST(JSON_VALUE(content, '$.id_ordem_pagamento') AS STRING) AS id_ordem_pagamento,
     SAFE_CAST(JSON_VALUE(content, '$.qtd_debito') AS INTEGER) AS qtd_debito,
     SAFE_CAST(JSON_VALUE(content, '$.qtd_gratuidade') AS INTEGER) AS qtd_gratuidade,
     SAFE_CAST(JSON_VALUE(content, '$.qtd_integracao') AS INTEGER) AS qtd_integracao,
@@ -29,18 +29,18 @@ WITH ordem_pagamento AS (
     SAFE_CAST(JSON_VALUE(content, '$.valor_taxa') AS FLOAT64) AS valor_taxa,
     SAFE_CAST(JSON_VALUE(content, '$.valor_vendaabordo') AS FLOAT64) AS valor_vendaabordo
   FROM
-    {{ source("br_rj_riodejaneiro_bilhetagem_staging", "ordem_pagamento") }}
+      {{ source("br_rj_riodejaneiro_bilhetagem_staging", "ordem_pagamento_consorcio") }}
 ),
-ordem_pagamento_rn AS (
+ordem_pagamento_consorcio_rn AS (
   SELECT
-    *,
-    ROW_NUMBER() OVER (PARTITION BY id_ordem_pagamento ORDER BY timestamp_captura DESC) AS rn
+      *,
+      ROW_NUMBER() OVER (PARTITION BY id_ordem_pagamento_consorcio ORDER BY timestamp_captura DESC) AS rn
   FROM
-    ordem_pagamento
+      ordem_pagamento_consorcio
 )
 SELECT
   * EXCEPT(rn)
 FROM
-  ordem_pagamento_rn
+  ordem_pagamento_consorcio_rn
 WHERE
   rn = 1
