7862e82a29c6d59ea3d05319ac101e26f3cd4764
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-03-29 19:31:14 -0300

    adiciona data de vistoria no licenciamento

--- models/veiculo/sppo_licenciamento.sql
+++ models/veiculo/sppo_licenciamento.sql
@@ -1,3 +1,4 @@
+-- depends_on: {{ ref('aux_sppo_licenciamento_vistoria_atualizada') }}
 {{
     config(
         materialized="incremental",
@@ -18,8 +19,13 @@ with
         {% if var("stu_data_versao") != "" %}
             data = date("{{ var('stu_data_versao') }}")
         {% else %}
-            {% if execute %}
-                {% set licenciamento_date = run_query("SELECT MIN(data) FROM " ~ ref("sppo_licenciamento_stu") ~ " WHERE data >= DATE_ADD(DATE('" ~ var("run_date") ~ "'), INTERVAL 5 DAY)").columns[0].values()[0] %}
+            -- Versão fixa do STU em 2024-03-25 devido à falha de atualização na fonte da dados (SIURB)
+            {%- if var("run_date") >= "2024-03-01" %}
+                {% set licenciamento_date = "2024-03-25" %}
+            {% else %}
+                {% if execute %}
+                    {% set licenciamento_date = run_query("SELECT MIN(data) FROM " ~ ref("sppo_licenciamento_stu") ~ " WHERE data >= DATE_ADD(DATE('" ~ var("run_date") ~ "'), INTERVAL 5 DAY)").columns[0].values()[0] %}
+                {% endif %}
             {% endif %}
 
             data = DATE("{{ licenciamento_date }}")
@@ -29,14 +35,50 @@ with
     stu_rn AS (
         select
             * except (timestamp_captura),
+            EXTRACT(YEAR FROM data_ultima_vistoria) AS ano_ultima_vistoria,
             ROW_NUMBER() OVER (PARTITION BY data, id_veiculo) rn
         from
             stu
+    ),
+    stu_ano_ultima_vistoria AS (
+        -- Temporariamente considerando os dados de vistoria enviados pela TR/SUBTT/CGLF
+        {% if var("run_date") >= "2024-03-01" %}
+        SELECT
+            s.* EXCEPT(ano_ultima_vistoria),
+            CASE 
+                WHEN c.ano_ultima_vistoria > s.ano_ultima_vistoria THEN c.ano_ultima_vistoria
+                ELSE COALESCE(s.ano_ultima_vistoria, c.ano_ultima_vistoria)
+            END AS ano_ultima_vistoria_atualizado,
+        FROM
+            stu_rn AS s
+        LEFT JOIN
+            (
+                SELECT
+                    id_veiculo, 
+                    placa,
+                    ano_ultima_vistoria
+                FROM
+                    {{ ref("aux_sppo_licenciamento_vistoria_atualizada") }}
+            ) AS c
+        USING
+            (id_veiculo, placa)
+        {% else %}
+        SELECT
+            s.* EXCEPT(ano_ultima_vistoria),
+            s.ano_ultima_vistoria AS ano_ultima_vistoria_atualizado,
+        FROM
+            stu_rn AS s
+        {% endif %}
     )
 select
-  * except(rn)
+  * except(rn, data_inicio_vinculo), 
+  CASE 
+    WHEN ano_ultima_vistoria_atualizado >= CAST(EXTRACT(YEAR FROM DATE_SUB("{{ var('run_date') }}", INTERVAL {{ var('sppo_licenciamento_validade_vistoria_ano') }} YEAR)) AS INT64) THEN TRUE -- Última vistoria realizada dentro do período válido
+    WHEN data_ultima_vistoria IS NULL AND DATE_DIFF(DATE("{{ var('run_date') }}"), data_inicio_vinculo, DAY) <=  {{ var('sppo_licenciamento_tolerancia_primeira_vistoria_dia') }} THEN TRUE -- Caso o veículo seja novo, existe a tolerância de 15 dias para a primeira vistoria
+  ELSE FALSE
+  END AS indicador_vistoria_valida,
 from
-  stu_rn
+  stu_ano_ultima_vistoria
 where
   rn = 1
 
--- models/veiculo/sppo_licenciamento_stu.sql
+++ models/veiculo/sppo_licenciamento_stu.sql
@@ -3,9 +3,14 @@ SELECT
    SAFE_CAST(DATETIME(TIMESTAMP_TRUNC(TIMESTAMP(timestamp_captura), SECOND), "America/Sao_Paulo" ) AS DATETIME) timestamp_captura,
    SAFE_CAST(JSON_VALUE(content,"$.modo") AS STRING) modo,
    SAFE_CAST(id_veiculo AS STRING) id_veiculo,
-   SAFE_CAST(JSON_VALUE(content,"$.ano_fabricacao") AS STRING) ano_fabricacao,
+   SAFE_CAST(JSON_VALUE(content,"$.ano_fabricacao") AS INT64) ano_fabricacao,
    SAFE_CAST(JSON_VALUE(content,"$.carroceria") AS STRING) carroceria,
-   SAFE_CAST(JSON_VALUE(content,"$.data_ultima_vistoria") AS STRING) data_ultima_vistoria,
+   CASE
+    WHEN JSON_VALUE(content,"$.data_ultima_vistoria") = "" THEN NULL
+   ELSE
+    SAFE_CAST(PARSE_DATETIME("%d/%m/%Y", JSON_VALUE(content,"$.data_ultima_vistoria")) AS DATE)
+   END
+   AS data_ultima_vistoria,
    SAFE_CAST(JSON_VALUE(content,"$.id_carroceria") AS INT64) id_carroceria,
    SAFE_CAST(JSON_VALUE(content,"$.id_chassi") AS INT64) id_chassi,
    SAFE_CAST(JSON_VALUE(content,"$.id_fabricante_chassi") AS INT64) id_fabricante_chassi,
@@ -22,6 +27,12 @@ SELECT
    SAFE_CAST(JSON_VALUE(content,"$.quantidade_lotacao_sentado") AS INT64) quantidade_lotacao_sentado,
    SAFE_CAST(JSON_VALUE(content,"$.tipo_combustivel") AS STRING) tipo_combustivel,
    SAFE_CAST(JSON_VALUE(content,"$.tipo_veiculo") AS STRING) tipo_veiculo,
-   SAFE_CAST(JSON_VALUE(content,"$.status") AS STRING) status
+   SAFE_CAST(JSON_VALUE(content,"$.status") AS STRING) status,
+   CASE
+    WHEN JSON_VALUE(content,"$.data_inicio_vinculo") = "" THEN NULL
+    ELSE
+    SAFE_CAST(PARSE_DATETIME("%d/%m/%Y", JSON_VALUE(content,"$.data_inicio_vinculo")) AS DATE)
+  END
+    AS data_inicio_vinculo,
  FROM
      {{ var('sppo_licenciamento_stu_staging') }} as t
