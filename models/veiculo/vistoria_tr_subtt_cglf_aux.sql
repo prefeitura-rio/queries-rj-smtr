5fe11e9db77c9132edaca1a644bb557c1781712a
Rodrigo Cunha <66736583+eng-rodrigocunha@users.noreply.github.com>
2024-03-29 12:48:37 -0300

    Update models/veiculo/vistoria_tr_subtt_cglf_aux.sql
    Co-authored-by: Fernanda Scovino <fscovinom@gmail.com>

--- models/veiculo/vistoria_tr_subtt_cglf_aux.sql
+++ models/veiculo/vistoria_tr_subtt_cglf_aux.sql
@@ -4,7 +4,7 @@
   )
 }}
 
-/* Modelo para aglutinar todas os dados com fonte TR/SUBTT/CGLF */
+/* Dados auxiliares de vistoria de ônibus levantados pela Coordenadoria Geral de Licenciamento e Fiscalização (TR/SUBTT/CGLF), para atualização da data de última vistoria informada no sistema (STU). */
 
 SELECT
   data,
