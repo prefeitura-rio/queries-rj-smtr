0a81d78656e809fdc009eb221d463d11532d6597
vtr363 <victor.m.rocha.vr@hotmail.com>
2024-04-01 16:25:20 -0300

    altera valores de milhões para o valor completo

--- models/projeto_subsidio_sppo_encontro_contas/encontro_contas_consorcio_mes.sql
+++ models/projeto_subsidio_sppo_encontro_contas/encontro_contas_consorcio_mes.sql
@@ -6,16 +6,16 @@ SELECT
   FROM
     DATA) AS mes,
   consorcio,
-  ROUND(SUM(km_apurada)/POW(10,6), 2) km_apurada_milhoes,
-  ROUND(SUM(km_planejada)/POW(10,6), 2) km_planejada_milhoes,
-  ROUND(SUM(km_subsidiada)/POW(10,6), 2) km_subsidiada_milhoes,
-  ROUND(SUM(remuneracao_tarifaria)/POW(10,6), 2) remuneracao_tarifaria_milhoes,
-  ROUND(SUM(subsidio)/POW(10,6), 2) subsidio_milhoes,
-  ROUND(SUM(receita_aferida)/POW(10,6), 2) AS receita_aferida_milhoes,
-  ROUND(SUM(receita_esperada)/POW(10,6), 2) AS receita_esperada_milhoes,
-  ROUND(SUM(desconto)/POW(10,6), 2) AS desconto_milhoes,
-  ROUND(SUM(saldo)/POW(10, 6), 2) AS saldo_milhoes,
-  ROUND(SUM(quantidade_passageiros_total)/POW(10, 6), 2) AS quantidade_passageiros_total_milhoes,
+  ROUND(SUM(km_apurada), 2) km_apurada,
+  ROUND(SUM(km_planejada), 2) km_planejada,
+  ROUND(SUM(km_subsidiada), 2) km_subsidiada,
+  ROUND(SUM(remuneracao_tarifaria), 2) remuneracao_tarifaria,
+  ROUND(SUM(subsidio), 2) subsidio,
+  ROUND(SUM(receita_aferida), 2) AS receita_aferida,
+  ROUND(SUM(receita_esperada), 2) AS receita_esperada,
+  ROUND(SUM(desconto), 2) AS desconto,
+  ROUND(SUM(saldo), 2) AS saldo,
+  ROUND(SUM(quantidade_passageiros_total), 2) AS quantidade_passageiros_total,
 FROM
   {{ ref("encontro_contas_consorcio_dia") }}
 GROUP BY
