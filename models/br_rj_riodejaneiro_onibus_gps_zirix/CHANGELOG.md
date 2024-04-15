4d5a2af4bbe9cedaf2f2dc417341e9ec0fceb6b7
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2024-04-15 12:08:12 -0300

    [HOTFIX] Ajusta colunas data e hora da view sppo_realocacao da Zirix (#268)
    * cast data e hora realocação
    
    * linka pr no changelog

--- models/br_rj_riodejaneiro_onibus_gps_zirix/CHANGELOG.md
+++ models/br_rj_riodejaneiro_onibus_gps_zirix/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog - onibus_gps_zirix
 
+## [1.0.1] - 2024-04-15
+
+### Alterado
+
+- Converte colunas de data e hora do modelo `sppo_realocacao_zirix` para o tipo adequado (https://github.com/prefeitura-rio/queries-rj-smtr/pull/268)
+
 ## [1.0.0] - 2024-04-11
 
 ### Adicionado
--- models/br_rj_riodejaneiro_onibus_gps_zirix/sppo_realocacao_zirix.sql
+++ models/br_rj_riodejaneiro_onibus_gps_zirix/sppo_realocacao_zirix.sql
@@ -17,7 +17,7 @@ SELECT
   SAFE_CAST(DATETIME(TIMESTAMP(datetime_saida), "America/Sao_Paulo") AS DATETIME) as datetime_saida,
   SAFE_CAST(DATETIME(TIMESTAMP(timestamp_processamento), "America/Sao_Paulo") AS DATETIME) as timestamp_processamento,
   SAFE_CAST(DATETIME(TIMESTAMP(timestamp_captura), "America/Sao_Paulo") AS DATETIME) as timestamp_captura,
-  data,
-  hora
+  SAFE_CAST(data AS DATE) as data,
+  SAFE_CAST(hora AS INT64) as hora
 FROM
   {{ source('br_rj_riodejaneiro_onibus_gps_zirix_staging', "realocacao") }}
