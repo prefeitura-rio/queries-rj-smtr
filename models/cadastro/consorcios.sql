e091e8f41b20dc95c272c368aef1a8edde7e16ca
Rafael <rafael.c.pinheiro96@gmail.com>
2024-04-16 17:09:23 -0300

    corrige ID consorcio da mobirio

--- models/cadastro/consorcios.sql
+++ models/cadastro/consorcios.sql
@@ -18,17 +18,28 @@ WITH stu AS (
       END AS cd_consorcio_jae
   FROM
     {{ ref("staging_operadora_empresa") }} AS stu
+  WHERE
+    perm_autor IN ({{ var("ids_consorcios").keys()|join(", ") }})
 )
 SELECT
   COALESCE(s.id_consorcio, j.cd_consorcio) AS id_consorcio,
-  j.nm_consorcio AS consorcio,
-  s.cnpj,
-  s.razao_social,
+  CASE
+    WHEN s.id_consorcio = '221000050' THEN "Consórcio BRT"
+    ELSE j.nm_consorcio 
+  END AS consorcio,
+  CASE
+    WHEN j.cd_consorcio = '1' THEN '44520687000161'
+    ELSE s.cnpj
+  END AS cnpj,
+  CASE
+    WHEN j.cd_consorcio = '1' THEN 'COMPANHIA MUNICIPAL DE TRANSPORTES COLETIVOS CMTC RIO'
+    ELSE s.razao_social
+  END AS razao_social,
   s.id_consorcio AS id_consorcio_stu,
   j.cd_consorcio AS id_consorcio_jae,
   s.processo AS id_processo,
 FROM {{ ref("staging_consorcio") }} AS j
-LEFT JOIN
+FULL OUTER JOIN
   stu AS s
 ON
   j.cd_consorcio = s.cd_consorcio_jae
