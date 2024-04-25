f4cc93af3c35347e994a90e69ece1706a0a3ad42
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-04-25 15:17:33 -0300

    Refatora e corrige conversão de data em get_license_date

--- macros/get_license_date.sql
+++ macros/get_license_date.sql
@@ -5,15 +5,15 @@ SELECT
     WHEN "{{ var('stu_data_versao') }}" != "" THEN DATE("{{ var('stu_data_versao') }}")
     {% endif %}
     --- Versão fixa do STU em 2024-03-25 para mar/Q1 devido à falha de atualização na fonte da dados (SIURB)
-    WHEN "{{ var('run_date') }}" >= "2024-03-01" AND "{{ var('run_date') }}" < "2024-03-16" THEN DATE("2024-03-25")
+    WHEN DATE("{{ var('run_date') }}") >= "2024-03-01" AND DATE("{{ var('run_date') }}") < "2024-03-16" THEN DATE("2024-03-25")
     -- Versão fixa do STU em 2024-04-09 para mar/Q2 devido à falha de atualização na fonte da dados (SIURB)
-    WHEN "{{ var('run_date') }}" >= "2024-03-16" AND "{{ var('run_date') }}" < "2024-04-01" THEN DATE("2024-04-09")
+    WHEN DATE("{{ var('run_date') }}") >= "2024-03-16" AND DATE("{{ var('run_date') }}") < "2024-04-01" THEN DATE("2024-04-09")
     ELSE (
-      SELECT MIN(data)
+      SELECT MIN(DATE(data))
       FROM {{ ref("sppo_licenciamento_stu_staging") }}
-      WHERE data >= DATE_ADD(DATE("{{ var('run_date') }}"), INTERVAL 5 DAY)
+      WHERE DATE(data) >= DATE_ADD(DATE("{{ var('run_date') }}"), INTERVAL 5 DAY)
         -- Admite apenas versões do STU igual ou após 2024-04-09 a partir de abril/24 devido à falha de atualização na fonte da dados (SIURB)
-        AND ("{{ var('run_date') }}" < "2024-04-01" OR data >= '2024-04-09')
+        AND (DATE("{{ var('run_date') }}") < "2024-04-01" OR DATE(data) >= '2024-04-09')
     )
   END
 {% endmacro %}
