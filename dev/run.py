1930145b61a13051b299c33642b9377639c49af0
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-04-29 09:47:35 -0300

    Revert "Merge branch 'master' into hotfix-subsidio-sumario"
    This reverts commit ff2a203c63b7b095b617371667f339c6d9b3a1f0, reversing
    changes made to c3c4869854a7e4f9c3d3b087f8c709994e81f60e.

--- dev/run.py
+++ dev/run.py
@@ -4,8 +4,6 @@ import os
 # Veja os parâmetros disponíveis da função run_dbt_model em util.py
 
 run_dbt_model(
-    dataset_id="dashboard_subsidio_sppo",
-    _vars={
-        "end_date": "2024-04-15"
-    }
+    dataset_id="gtfs",
+    flags="--full-refresh",
 )
