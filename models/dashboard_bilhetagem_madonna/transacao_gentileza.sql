a9890cf7a3ee906034066e5302161d806e65fa47
vtr363 <victor.m.rocha.vr@hotmail.com>
2024-05-02 19:13:52 -0300

    alteracoes adiciona dashboard_bilhetagem_madonna

---
+++ models/dashboard_bilhetagem_madonna/transacao_gentileza.sql
@@ -0,0 +1,25 @@
+
+WITH transacao_deduplicada AS (
+    SELECT 
+        * EXCEPT(rn)
+    FROM
+    (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY timestamp_captura DESC) AS rn
+        FROM
+            {{ ref('staging_transacao') }}
+        where
+            data in ("2024-05-02", "2024-05-03")
+    )
+    WHERE
+        rn = 1
+)
+SELECT
+  EXTRACT(DATE FROM data_transacao) AS data,
+  FORMAT_TIMESTAMP("%H:%M:%S", data_transacao) AS hora,
+  id as id_transacao  
+FROM
+  transacao_deduplicada
+WHERE
+    cd_linha = "1033"
---
+++ models/dashboard_bilhetagem_madonna/transacao_madonna.sql
@@ -0,0 +1,26 @@
+
+WITH transacao_deduplicada AS (
+    SELECT 
+        * EXCEPT(rn)
+    FROM
+    (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY timestamp_captura DESC) AS rn
+        FROM
+            {{ ref('staging_transacao') }}
+        where
+            data = "2024-05-04"
+    )
+    WHERE
+        rn = 1
+)
+SELECT
+  EXTRACT(DATE FROM data_transacao) AS data,
+  FORMAT_TIMESTAMP("%H:%M:%S", data_transacao) AS hora,
+  id as id_transacao  
+FROM
+  transacao_deduplicada
+WHERE
+  cd_operadora = "2359"
+  AND cd_linha = "1126"
