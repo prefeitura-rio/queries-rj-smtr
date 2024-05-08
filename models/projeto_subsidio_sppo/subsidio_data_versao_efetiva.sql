5ecda7c9be818f30c69a5d584506a72b4ca671b6
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-05-08 17:02:49 -0300

    Remove planejamento Madonna

--- models/projeto_subsidio_sppo/subsidio_data_versao_efetiva.sql
+++ models/projeto_subsidio_sppo/subsidio_data_versao_efetiva.sql
@@ -346,11 +346,12 @@ WITH
       WHEN data BETWEEN DATE(2024,04,15) AND DATE(2024,05,02) THEN "2024-04-15"  -- OS abr/Q2
       WHEN data BETWEEN DATE(2024,05,03) AND DATE(2024,05,31) THEN "2024-05-03"  -- OS maio/Q1
     END AS feed_version,
-    CASE
-      WHEN data = DATE(2024,05,04) THEN "Madonna 2024-05-04"
-      WHEN data = DATE(2024,05,05) THEN "Madonna 2024-05-05"
-      ELSE "Regular"
-    END AS tipo_os,
+    -- CASE
+    --   WHEN data = DATE(2024,05,04) THEN "Madonna 2024-05-04"
+    --   WHEN data = DATE(2024,05,05) THEN "Madonna 2024-05-05"
+    --   ELSE "Regular"
+    -- END AS tipo_os,
+    "Regular" AS tipo_os,
   FROM UNNEST(GENERATE_DATE_ARRAY("{{var('DATA_SUBSIDIO_V6_INICIO')}}", "2024-12-31")) AS data)
 SELECT
   data,
