944132d2608d5193f5e2ff1818c40ed79143c84b
Fernanda Scovino <fscovinom@gmail.com>
2024-05-16 16:20:03 -0300

    Atualiza schema do encontro contas (#306)
    * update tables name + config
    
    * update column name
    
    * add schema docs
    
    * update schema

--- models/projeto_subsidio_sppo_encontro_contas/balanco_servico_quinzena.sql
+++ models/projeto_subsidio_sppo_encontro_contas/balanco_servico_quinzena.sql
@@ -2,14 +2,14 @@ WITH
   q1 AS (
   SELECT
     FORMAT_DATE('%Y-%m-Q1', date) AS quinzena,
-    date AS data_inicio_quinzena,
+    date AS data_inicial_quinzena,
     DATE_ADD(date, INTERVAL 14 DAY) AS data_final_quinzena
   FROM
     UNNEST(GENERATE_DATE_ARRAY('2022-06-01', '2023-12-31', INTERVAL 1 MONTH)) AS date ),
   q2 AS (
   SELECT
     FORMAT_DATE('%Y-%m-Q2', date) AS quinzena,
-    DATE_ADD(date, INTERVAL 15 DAY) AS data_inicio_quinzena,
+    DATE_ADD(date, INTERVAL 15 DAY) AS data_inicial_quinzena,
     LAST_DAY(date) AS data_final_quinzena
   FROM
     UNNEST(GENERATE_DATE_ARRAY('2022-06-01', '2023-12-31', INTERVAL 1 MONTH)) AS date ),
@@ -24,10 +24,10 @@ WITH
   FROM
     q2
   ORDER BY
-    data_inicio_quinzena )
+    data_inicial_quinzena )
 SELECT
   quinzena,
-  data_inicio_quinzena,
+  data_inicial_quinzena,
   data_final_quinzena,
   consorcio,
   servico,
@@ -46,15 +46,15 @@ FROM
 LEFT JOIN
   {{ ref("balanco_servico_dia") }} bs
 ON
-  bs.data BETWEEN qz.data_inicio_quinzena
+  bs.data BETWEEN qz.data_inicial_quinzena
   AND qz.data_final_quinzena
 GROUP BY
   quinzena,
-  data_inicio_quinzena,
+  data_inicial_quinzena,
   data_final_quinzena,
   consorcio,
   servico
 ORDER BY
-  data_inicio_quinzena,
+  data_inicial_quinzena,
   consorcio,
   servico
