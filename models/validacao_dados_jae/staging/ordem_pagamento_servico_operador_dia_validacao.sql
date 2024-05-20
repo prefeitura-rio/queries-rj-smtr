1850b5dceb6ae29dd0584adb9f3a947a56fd0dad
Rafael <rafael.c.pinheiro96@gmail.com>
2024-05-20 19:15:46 -0300

    corrige alias

--- models/validacao_dados_jae/staging/ordem_pagamento_servico_operador_dia_validacao.sql
+++ models/validacao_dados_jae/staging/ordem_pagamento_servico_operador_dia_validacao.sql
@@ -145,7 +145,7 @@ transacao_ordem AS (
     op.valor_total_transacao_bruto,
     op.valor_total_transacao_liquido,
     t.quantidade_total_transacao_captura,
-    CAST(t.valor_total_transacao_captura+ op.valor_rateio_credito + op.valor_rateio_debito AS NUMERIC) AS valor_total_transacao_captura
+    CAST(t.valor_total_transacao_captura + op.valor_rateio_credito + op.valor_rateio_debito AS NUMERIC) AS valor_total_transacao_captura
   FROM
     ordem_pagamento op
   FULL OUTER JOIN
