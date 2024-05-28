54315c7929877e059ef7e5170589a2ab4870f7fc
Ashley Vicary <ashley.vicary@outlook.com>
2024-05-28 10:07:37 -0400

    Updated to include centriod column

--- models/mit_ipea_project/vw_h3.sql
+++ models/mit_ipea_project/vw_h3.sql
@@ -1,9 +1,7 @@
 -- H3 Resolution 8 view, convert geometry column to GEOGRAPHY type
-
-SELECT 
-    tile_id,
-    resolution,
-    parent_id,
-    ST_GEOGFROMTEXT(geometry) AS geometry -- Convert string in GEOGRAPHY value
-FROM 
-    `rj-smtr.br_rj_riodejaneiro_geo.h3_res8`
+SELECT  tile_id,
+        resolution,
+        parent_id,
+        ST_GEOGFROMTEXT(geometry) AS geometry, -- Convert string in GEOGRAPHY value
+        ST_CENTROID(ST_GEOGFROMTEXT(geometry)) AS centroid
+FROM `rj-smtr.br_rj_riodejaneiro_geo.h3_res8`
