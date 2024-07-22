45d7472fe444fed77c997b04fbb3192c3505408e
Ashley Vicary <ashley.vicary@outlook.com>
2024-07-22 15:06:14 -0400

    Update origin_destination_land_use.sql

--- models/mit_ipea_project/origin_destination_land_use.sql
+++ models/mit_ipea_project/origin_destination_land_use.sql
@@ -31,11 +31,15 @@ WITH ticketing AS (
 
     FROM `rj-smtr-dev`.mit_ipea_project.vw_ticketing
     -- In future remove hardcoded date
-    WHERE as_at BETWEEN '2023-06-02' AND '2023-10-01'
+    WHERE as_at BETWEEN '2023-01-01' AND '2023-01-01'
+    --WHERE as_at IN (
+    --            '2023-06-02'
+    ----, '2023-06-03'
+    --           )
 ),
 
 -- CTE 2: Estimate OD for each transaction
-origin_destination AS (
+od_prediction AS (
 
 SELECT
     card_id,
@@ -55,9 +59,11 @@ SELECT
     h3_bus_sitting.tile_id AS destination_tile,
     h3_bus_sitting.centroid AS destination_centroid,
     daily_trip_stage,
+    EXP(ln_CMATT30) AS CMATT30,
     ST_DISTANCE(h3_next_transaction.centroid, h3_bus_sitting.centroid) AS distance_from_next_transaction,
     MIN(ST_DISTANCE(h3_next_transaction.centroid, h3_bus_sitting.centroid))
-        OVER(PARTITION BY card_id, ticketing.as_at, daily_trip_id) AS exit_prediction
+        OVER(PARTITION BY card_id, ticketing.as_at, daily_trip_id) AS exit_prediction,
+    ROW_NUMBER() OVER(PARTITION BY card_id, ticketing.as_at, daily_trip_id ORDER BY h3_bus_sitting.tile_entry_time) AS row_num
 
 FROM ticketing
 
@@ -66,8 +72,8 @@ LEFT JOIN `rj-smtr-dev`.mit_ipea_project.h3_gps AS h3_next_transaction
     ON
         ticketing.next_transaction_vehicle_id   = RIGHT(h3_next_transaction.vehicle_id, 5)
     AND ticketing.as_at                         = h3_next_transaction.as_at
-    AND ticketing.next_transaction_time >= h3_next_transaction.tile_entry_time
-    AND ticketing.next_transaction_time < h3_next_transaction.tile_exit_time
+    AND ticketing.next_transaction_time         >= h3_next_transaction.tile_entry_time
+    AND ticketing.next_transaction_time         < h3_next_transaction.tile_exit_time
 -- BOARDING LOCATION
 LEFT JOIN `rj-smtr-dev`.mit_ipea_project.h3_gps AS h3_boarding
     ON
@@ -84,11 +90,75 @@ LEFT JOIN `rj-smtr-dev`.mit_ipea_project.h3_gps AS h3_bus_sitting
     AND ticketing.transaction_time NOT BETWEEN h3_bus_sitting.tile_entry_time AND h3_bus_sitting.tile_exit_time
     AND h3_bus_sitting.tile_entry_time BETWEEN ticketing.transaction_time
         AND TIME_ADD(ticketing.transaction_time, INTERVAL 2 HOUR)
+LEFT JOIN `rj-smtr-dev`.mit_ipea_project.accessibility_jobs
+    ON h3_bus_sitting.tile_id = accessibility_jobs.h3_address
 
-)
+),
 
--- Output: UNION ALL
---Transaction with more than one daily trip stage
+min_exit_row AS (
+-- May be multiple exit rows
+    SELECT *,
+           CASE
+               WHEN distance_from_next_transaction = exit_prediction THEN 1
+               ELSE 0
+           END AS exit_row
+    FROM od_prediction
+    ),
+
+exit_row_num AS (SELECT *,
+                        MIN(CASE WHEN exit_row = 1 THEN row_num END)
+                            OVER (PARTITION BY as_at, card_id, daily_trip_id) AS min_row_num_with_exit_row_1,
+                        MIN(CASE WHEN exit_row = 1 THEN destination_time1 END)
+                            OVER (PARTITION BY as_at, card_id, daily_trip_id) AS trip_chaining_destination_time,
+                     CASE
+                         WHEN distance_from_next_transaction = 0 THEN CMATT30
+                        ELSE CMATT30 * (1/distance_from_next_transaction)
+                         END AS land_use_score,
+                    CASE
+                        WHEN
+                          TIME_DIFF(
+                                destination_time1,
+                                MIN(CASE WHEN exit_row = 1 THEN destination_time1 END)
+                                    OVER (PARTITION BY as_at, card_id, daily_trip_id),
+                                MINUTE) = 0 THEN CMATT30
+                        ELSE CMATT30 * (
+                            1/
+                            POWER(ABS(TIME_DIFF(
+                                destination_time1,
+                                MIN(CASE WHEN exit_row = 1 THEN destination_time1 END)
+                                    OVER (PARTITION BY as_at, card_id, daily_trip_id),
+                                MINUTE)
+                            ), 0.5)
+                            )
+                         END AS land_use_score_time
+                 FROM min_exit_row
+                 ),
+
+land_use_time_estimate AS (
+    SELECT *,
+          CASE
+              WHEN
+                  land_use_score_time =
+                  MAX(land_use_score_time) OVER (PARTITION BY as_at, card_id, daily_trip_id)
+                  THEN 1
+              ELSE 0
+              END AS exit_row_land_use_time
+   FROM exit_row_num
+   WHERE row_num BETWEEN min_row_num_with_exit_row_1 - 15 AND min_row_num_with_exit_row_1 + 15
+     --AND card_id IN ('000419a63840109bb1f656d75cd2c8f48f8ea90a4a72124f421ad8962094ad12'
+    --                 'caf2a50e1ba90fde80690abcc9efc62ca1747702afa20761ecce65d68979b3fd',
+    --                 'b76259e138c3d18c1f8e6e198ec91482dd257545a11c3af6addbd8d97d50dde8'
+    --   )
+--AND daily_trip_id = 3
+),
+
+--SELECT *
+--FROM land_use_time_estimate
+--WHERE exit_row_land_use_time = 1 --xOR row_num = min_row_num_with_exit_row_1
+--ORDER BY as_at, card_id, daily_trip_id
+
+origin_destination AS (
+
 SELECT
     card_id,
     as_at,
@@ -97,19 +167,50 @@ SELECT
     daily_trip_id,
     transaction_vehicle_id,
     boarding_tile,
-    MIN(destination_time1) AS destination_time1,
-    MIN(destination_time2) AS destination_time2,
+    destination_time1 AS destination_time1,
+    destination_time2 AS destination_time2,
     destination_tile,
     next_transaction_time,
     next_transaction_tile_id,
-    AVG(ST_DISTANCE(boarding_tile_centriod, destination_centroid)) AS distance_travelled_linear
-FROM origin_destination
+    CMATT30,
+    ST_DISTANCE(boarding_tile_centriod, destination_centroid) AS distance_travelled_linear,
+    distance_from_next_transaction,
+    exit_prediction AS distance_transit,
+    row_num,
+    exit_row,
+    exit_row_land_use_time
+FROM land_use_time_estimate
 
-WHERE distance_from_next_transaction = exit_prediction
+WHERE exit_row_land_use_time = 1
     AND daily_trip_stage != 'Only transaction'
 
-GROUP BY card_id, as_at, transaction_time, daily_trip_stage, daily_trip_id, transaction_vehicle_id, boarding_tile,
-         destination_tile, next_transaction_time, next_transaction_tile_id
+),
+
+-- Output: UNION ALL
+--Transaction with more than one daily trip stage
+output AS (
+
+    SELECT
+        card_id,
+        as_at,
+        transaction_time,
+        daily_trip_stage,
+        daily_trip_id,
+        transaction_vehicle_id,
+        boarding_tile,
+        destination_time1,
+        destination_time2,
+        destination_tile,
+        next_transaction_time,
+        next_transaction_tile_id,
+        CMATT30,
+        distance_travelled_linear,
+        distance_from_next_transaction,
+        distance_transit,
+        row_num,
+        exit_row,
+        exit_row_land_use_time
+    FROM origin_destination
 
 UNION ALL
 
@@ -129,7 +230,19 @@ SELECT
     CAST(NULL AS STRING) AS destination_tile,
     CAST(NULL AS TIME) AS next_transaction_time,
     CAST(NULL AS STRING) AS next_transaction_tile_id,
-    NULL AS distance_travelled_linear
-FROM origin_destination
+    NULL AS CMATT30,
+    NULL AS distance_travelled_linear,
+    NULL AS distance_from_next_transaction,
+    NULL AS distance_transit,
+    NULL AS row_num,
+    NULL AS exit_row,
+    NULL AS exit_row_land_use_time
+FROM od_prediction
 WHERE daily_trip_stage = 'Only transaction'
   OR distance_from_next_transaction IS NULL
+)
+
+SELECT
+    *
+FROM output
+
